533f174dfeb6b367412c633971f9f58b90347044
Agnieszka Kurek <agniechakurek@gmail.com>
2017-02-15 18:54:49 +0100

    add function formatDate

--- basic/exercise1/script.js
+++ basic/exercise1/script.js
@@ -1,24 +1,43 @@
 'use strict';
+function formatDate(date) {
+    var aDay = date.getDate();
+    var Month = date.getMonth();
+    var Year = date.getFullYear();
+    var thisDate = `${aDay}:${Month}:${Year}`;
+    var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    var weekDays = `${days[date.getDay()]}`;  
+    var hours = date.getHours();
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    if (hours < 10) {
+        hours = "0" + hours;
+    };
+    var minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    };
+
+    var seconds = date.getSeconds();
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    };
+
+    var time = `${weekDays} ${hours} ${ampm} ${minutes}: ${seconds}`;
+    return time;
+    
+};
+
+var previousDate = new Date(2017, 2, 2, 2, 2, 2);
+var formattedPreviousDate = formatDate(previousDate);
+document.getElementById("previousDate").innerHTML = formattedPreviousDate;
+console.log(formattedPreviousDate);
+
 var currentDate = new Date();
-var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-var weekDays = `Today is : ${days[currentDate.getDay()]}`;
-document.getElementById("thisDay").innerHTML = weekDays;
+var formattedDate = formatDate(currentDate);
+document.getElementById("currentDate").innerHTML = formattedDate;
+console.log(formattedDate);
 
-var hours = currentDate.getHours();
-var ampm = hours >= 12 ? 'PM' : 'AM';
-hours = hours % 12;
-if (hours < 10) {
-    hours = "0" + hours;
-};
-var minutes = currentDate.getMinutes();
-if (minutes < 10) {
-    minutes = "0" + minutes;
-};
-
-var seconds = currentDate.getSeconds();
-if (seconds < 10) {
-    seconds = "0" + seconds;
-};
-
-var time = `${hours} ${ampm} ${minutes}: ${seconds}`;
-document.getElementById("thisTime").innerHTML = time;
+var nextDate = new Date(2017, 11, 11, 11, 11, 11);
+var formattedNextDate = formatDate(nextDate);
+document.getElementById("nextDate").innerHTML = formattedNextDate;
+console.log(formattedNextDate);
