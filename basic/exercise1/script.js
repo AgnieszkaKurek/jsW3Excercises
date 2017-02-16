bae861cf8a6cfd1b23193b75944bfb74a38719dd
Agnieszka Kurek <agniechakurek@gmail.com>
2017-02-16 13:21:07 +0100

    rename and formate function

--- basic/exercise1/script.js
+++ basic/exercise1/script.js
@@ -1,22 +1,19 @@
 'use strict';
-function formatDate(date) {
-   
-    var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-    var weekDays = `${days[date.getDay()]}`;
-    var hours = insertingZero(date.getHours());
-    var ampm = hours >= 12 ? 'PM' : 'AM';
-    hours = hours % 12;
-
-    var minutes = insertingZero(date.getMinutes());
-    var seconds = insertingZero(date.getSeconds());
-    var time = `${weekDays} ${hours} ${ampm} ${minutes}: ${seconds}`;
-    return time;
+function formatAsTwoDigits(timeUnit) {
+    return (timeUnit < 10) ? '0' + timeUnit : timeUnit;
 };
 
- function insertingZero(timeUnit) {
-        return (timeUnit < 10) ? '0' + timeUnit : timeUnit;
-    };
-
+function formatDate(date) {
+    var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    var weekDays = `${days[date.getDay()]}`;
+    var hours = date.getHours();
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    var minutes = formatAsTwoDigits(date.getMinutes());
+    var seconds = formatAsTwoDigits(date.getSeconds());
+    var time = `${weekDays} ${formatAsTwoDigits(hours)} ${ampm} ${minutes}: ${seconds}`;
+    return time;
+};
 var previousDate = new Date(2017, 2, 2, 2, 2, 2);
 var formattedPreviousDate = formatDate(previousDate);
 document.getElementById("previousDate").innerHTML = formattedPreviousDate;
