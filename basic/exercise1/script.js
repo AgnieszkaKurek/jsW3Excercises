94ffcbda26245e3ef09341a46cf18b4845b6e992
Agnieszka Kurek <agniechakurek@gmail.com>
2017-02-16 10:29:14 +0100

    add  function insertingZero

--- basic/exercise1/script.js
+++ basic/exercise1/script.js
@@ -1,28 +1,19 @@
 'use strict';
 function formatDate(date) {
+    function insertingZero(i) {
+        return (i < 10) ? '0' + i : i;
+    };
 
     var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
     var weekDays = `${days[date.getDay()]}`;
-    var hours = date.getHours();
+    var hours = insertingZero(date.getHours());
     var ampm = hours >= 12 ? 'PM' : 'AM';
     hours = hours % 12;
-    if (hours < 10) {
-        hours = "0" + hours;
-    };
 
-    var minutes = date.getMinutes();
-    if (minutes < 10) {
-        minutes = "0" + minutes;
-    };
-
-    var seconds = date.getSeconds();
-    if (seconds < 10) {
-        seconds = "0" + seconds;
-    };
-
+    var minutes = insertingZero(date.getMinutes());
+    var seconds = insertingZero(date.getSeconds());
     var time = `${weekDays} ${hours} ${ampm} ${minutes}: ${seconds}`;
     return time;
-
 };
 
 var previousDate = new Date(2017, 2, 2, 2, 2, 2);
