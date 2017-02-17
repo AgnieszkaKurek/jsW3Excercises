4317b5a5e244aaed8aa65ff9af6b5ba1db8ef4ea
Agnieszka Kurek <agniechakurek@gmail.com>
2017-02-17 10:58:26 +0100

    remove empty line and rename function

--- basic/exercise1/script.js
+++ basic/exercise1/script.js
@@ -16,13 +16,12 @@ function formatDate(date) {
     var time = `${formatWeekDays} ${formatHours} ${ampm} ${formatMinutes}: ${formatSeconds}`;
     return time;
 };
-function formatshortDate(date) {
+function formatShortDate(date) {
     var month = formatAsTwoDigits(date.getMonth());
     var day = formatAsTwoDigits(date.getDay());
     var year = date.getFullYear();
     var shortDate = `${month}-${day}-${year}`;
     return shortDate;
-
 };
 
 function formatAndDisplayDate(date, id, formatFunction) {
@@ -30,10 +29,10 @@ function formatAndDisplayDate(date, id, formatFunction) {
     document.getElementById(id).innerHTML = fdate;
 };
 
-formatAndDisplayDate(new Date(1986, 4, 2, 5, 7, 6), "Date1", formatshortDate);
-formatAndDisplayDate(new Date(2009, 09, 20, 9, 10, 2), "Date2", formatshortDate);
+formatAndDisplayDate(new Date(1986, 4, 2, 5, 7, 6), "Date1", formatShortDate);
+formatAndDisplayDate(new Date(2009, 09, 20, 9, 10, 2), "Date2", formatShortDate);
 formatAndDisplayDate(new Date(2017, 2, 2, 2, 2, 2), "Date3", formatDate);
 formatAndDisplayDate(new Date(), "Date4", formatDate);
 formatAndDisplayDate(new Date(2017, 11, 11, 11, 11, 11), "Date5", formatDate);
-formatAndDisplayDate(new Date(2020, 2, 6, 2, 22, 20), "Date6", formatshortDate);
-formatAndDisplayDate(new Date(2100, 1, 1, 1, 1, 1), "Date7", formatshortDate);
+formatAndDisplayDate(new Date(2020, 2, 6, 2, 22, 20), "Date6", formatShortDate);
+formatAndDisplayDate(new Date(2100, 1, 1, 1, 1, 1), "Date7", formatShortDate);
