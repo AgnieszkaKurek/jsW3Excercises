dc2dd4fa89730dcda5193a0f7a2157ce61f0372f
Agnieszka Kurek <agniechakurek@gmail.com>
2017-02-17 13:09:33 +0100

    find the area of a triangle

--- basic/exercise2/script2.js
+++ basic/exercise2/script2.js
@@ -1,1 +1,10 @@
 'use strict';
+
+var side1 = 5;
+var side2 = 6;
+var side3 = 7
+var trianglePerimeter = (side1 + side2 + side3) / 2;
+var triangleArea = Math.sqrt(trianglePerimeter * ((trianglePerimeter - side1) * (trianglePerimeter - side2) * (trianglePerimeter - side3)));
+document.getElementById("triangle").innerHTML = triangleArea;
+
+
