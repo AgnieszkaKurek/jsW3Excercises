d6c96fa80b5b882b0f530d79a96e5e0f9a96dafd
Agnieszka Kurek <agniechakurek@gmail.com>
2017-02-20 10:39:49 +0100

    add values trianglePerSide

--- basic/exercise2/script2.js
+++ basic/exercise2/script2.js
@@ -2,9 +2,10 @@
 
 var side1 = 5;
 var side2 = 6;
-var side3 = 7
+var side3 = 7;
 var trianglePerimeter = (side1 + side2 + side3) / 2;
-var triangleArea = Math.sqrt(trianglePerimeter * ((trianglePerimeter - side1) * (trianglePerimeter - side2) * (trianglePerimeter - side3)));
+var trianglePerSide1 = trianglePerimeter - side1;
+var trianglePerSide2 = trianglePerimeter - side2;
+var trianglePerSide3 = trianglePerimeter - side3;
+var triangleArea = Math.sqrt(trianglePerimeter * trianglePerSide1 * trianglePerSide2 * trianglePerSide3 );
 document.getElementById("triangle").innerHTML = triangleArea;
-
-
