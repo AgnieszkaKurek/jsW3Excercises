a4709e420df45d56c7c78d6279569af9fbbd4cc7
Agnieszka Kurek <agniechakurek@gmail.com>
2017-02-20 13:35:56 +0100

    add throw exception(if sides are less than 0 shows an error message)

--- basic/exercise2/script2.js
+++ basic/exercise2/script2.js
@@ -1,6 +1,16 @@
 'use strict';
 
 function findingTriangleArea(side1, side2, side3) {
+    if (!(side1 > 0)) {
+        throw new Error("side1 should be a longer than 0");
+    };
+    if (!(side2 > 0)) {
+        throw new Error("side2 should be a longer than 0");
+    };
+    if (!(side3 > 0)) {
+        throw new Error("side3 should be a longer than 0");
+    };
+
     var trianglePerimeter = (side1 + side2 + side3) / 2;
     var trianglePerSide1 = trianglePerimeter - side1;
     var trianglePerSide2 = trianglePerimeter - side2;
