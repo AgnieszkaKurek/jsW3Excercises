49f7e0a649808a3dfa4598a1aa200330bc31b9b6
AgnieszkaKurek <agniechakurek@gmail.com>
2017-02-20 21:05:20 +0100

    Feature 9 (#13)
    * add values trianglePerSide
    
    * add function findingTriangleArea
    
    * add throw exception(if sides are less than 0 shows an error message)
    
    * rename function  and delete unnecessary code
    
    * add function validateSide
    
    * delete empty lines

--- basic/exercise2/script2.js
+++ basic/exercise2/script2.js
@@ -1,10 +1,21 @@
 'use strict';
 
-var side1 = 5;
-var side2 = 6;
-var side3 = 7
-var trianglePerimeter = (side1 + side2 + side3) / 2;
-var triangleArea = Math.sqrt(trianglePerimeter * ((trianglePerimeter - side1) * (trianglePerimeter - side2) * (trianglePerimeter - side3)));
-document.getElementById("triangle").innerHTML = triangleArea;
+function validateSide(side) {
+    if (side <= 0) {
+        throw new Error("All sides should be bigger than 0");
+    };
+};
 
+function triangleArea(side1, side2, side3) {
+    validateSide(side1);
+    validateSide(side2);
+    validateSide(side3);
+    var trianglePerimeter = (side1 + side2 + side3) / 2;
+    var trianglePerSide1 = trianglePerimeter - side1;
+    var trianglePerSide2 = trianglePerimeter - side2;
+    var trianglePerSide3 = trianglePerimeter - side3;
+    var triangleAreaMath = Math.sqrt(trianglePerimeter * trianglePerSide1 * trianglePerSide2 * trianglePerSide3);
+    return triangleAreaMath;
+};
 
+document.getElementById("triangle").innerHTML = triangleArea(5, 6, 7);
