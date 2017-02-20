8b29d0812005f5ddae1bcadfb960e334d4a6dd40
Agnieszka Kurek <agniechakurek@gmail.com>
2017-02-20 12:05:40 +0100

    add function findingTriangleArea

--- basic/exercise2/script2.js
+++ basic/exercise2/script2.js
@@ -1,11 +1,13 @@
 'use strict';
 
-var side1 = 5;
-var side2 = 6;
-var side3 = 7;
-var trianglePerimeter = (side1 + side2 + side3) / 2;
-var trianglePerSide1 = trianglePerimeter - side1;
-var trianglePerSide2 = trianglePerimeter - side2;
-var trianglePerSide3 = trianglePerimeter - side3;
-var triangleArea = Math.sqrt(trianglePerimeter * trianglePerSide1 * trianglePerSide2 * trianglePerSide3 );
-document.getElementById("triangle").innerHTML = triangleArea;
+function findingTriangleArea(side1, side2, side3) {
+    var trianglePerimeter = (side1 + side2 + side3) / 2;
+    var trianglePerSide1 = trianglePerimeter - side1;
+    var trianglePerSide2 = trianglePerimeter - side2;
+    var trianglePerSide3 = trianglePerimeter - side3;
+    var triangleAreaMath = Math.sqrt(trianglePerimeter * trianglePerSide1 * trianglePerSide2 * trianglePerSide3);
+    return triangleAreaMath;
+};
+
+var triangleArea = findingTriangleArea;
+document.getElementById("triangle").innerHTML = triangleArea(5, 6, 7);
