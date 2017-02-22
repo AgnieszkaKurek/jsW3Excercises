37c71a3d1d7032b682bc5d275d6002559c20cc45
AgnieszkaKurek <agniechakurek@gmail.com>
2017-02-22 21:42:38 +0100

    animation string (#16)
    * animation string
    
    * rename id and var
    
    * renema string.split to text.split
    
    * add empty line

--- basic/exercise3/script3.js
+++ basic/exercise3/script3.js
@@ -1,1 +1,10 @@
 'use strict';
+
+var text = document.getElementById("textAnimation").innerHTML;
+var characters = text.split("");
+
+setInterval(function () {
+    var firstCharacter = characters.shift();
+    characters.push(firstCharacter);
+    document.getElementById("textAnimation").innerHTML = characters.join("");
+}, 500);
