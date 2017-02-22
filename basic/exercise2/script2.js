06599a24dca36b99639a5eda49df8b633c1e3c56
Agnieszka Kurek <agniechakurek@gmail.com>
2017-02-22 20:58:19 +0100

    add validateAnyTriangleSide

--- basic/exercise2/script2.js
+++ basic/exercise2/script2.js
@@ -8,17 +8,15 @@ function validateSide(side) {
         throw new Error("All sides should be bigger than 0");
     };
 };
-
+function validateAnyTriangleSide(propablyLongestSide, propablyShortestSide1, propablyShortestSide2) {
+    if (propablyLongestSide > propablyShortestSide1 + propablyShortestSide2) {
+        throw new Error("One side should be smaller than sum of two other sides");
+    };
+}
 function triangleArea(side1, side2, side3) {
-    if (side1 > side2 + side3) {
-        throw new Error("One side should be smaller than sum of two consecutive sides")
-    };
-    if (side2 > side1 + side3) {
-        throw new Error("One side should be smaller than sum of two consecutive sides")
-    };
-    if (side3 > side2 + side1) {
-        throw new Error("One side should be smaller than sum of two consecutive sides")
-    };
+    validateAnyTriangleSide(side1, side2, side3);
+    validateAnyTriangleSide(side2, side1, side3);
+    validateAnyTriangleSide(side3, side2, side1);
     validateSide(side1);
     validateSide(side2);
     validateSide(side3);
@@ -30,4 +28,4 @@ function triangleArea(side1, side2, side3) {
     return triangleAreaMath;
 };
 
-document.getElementById("triangle").innerHTML = triangleArea(5, 6, 7);
+document.getElementById("triangle").innerHTML = triangleArea(5, 6, 17);
