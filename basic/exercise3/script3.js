06d7974a470d39d70de6a7e7967058f231adcc12
Agnieszka Kurek <agniechakurek@gmail.com>
2017-02-22 20:17:33 +0100

    animation string

--- basic/exercise3/script3.js
+++ basic/exercise3/script3.js
@@ -1,1 +1,10 @@
 'use strict';
+
+var string = document.getElementById("inscription").innerHTML;
+var characters = string.split("");
+setInterval(function () {
+    var firstCharacter = characters.shift();
+    characters.push(firstCharacter);
+    document.getElementById("inscription").innerHTML = characters.join("");
+}, 500);
+
