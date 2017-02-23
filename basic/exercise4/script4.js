6c97aaa5b3648c7a62798ceea78e030f1c7d609c
AgnieszkaKurek <agniechakurek@gmail.com>
2017-02-23 15:35:18 +0100

    add function leapYear  - determine whether a given year is a leap year in the Gregorian calendar

--- basic/exercise4/script4.js
+++ basic/exercise4/script4.js
@@ -1,1 +1,10 @@
 'use strict';
+
+function leapYear(year){
+  return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+};
+
+document.getElementById("leapYear1").innerHTML = leapYear(1986);
+document.getElementById("leapYear2").innerHTML = leapYear(2008);
+document.getElementById("leapYear3").innerHTML = leapYear(2000);
+
