6f6b3f653d33c2b6003832b9b80282b4360b78d6
AgnieszkaKurek <agniechakurek@gmail.com>
2017-02-23 15:54:59 +0100

    rename fun and sign changection

--- basic/exercise4/script4.js
+++ basic/exercise4/script4.js
@@ -1,9 +1,9 @@
 'use strict';
 
-function leapYear(year) {
-    return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+function isLeapYear(year) {
+    return ((year % 4 === 0) && (year % 100 != 0)) || (year % 400 === 0);
 };
 
-document.getElementById("leapYear1").innerHTML = leapYear(1986);
-document.getElementById("leapYear2").innerHTML = leapYear(2008);
-document.getElementById("leapYear3").innerHTML = leapYear(2000);
+document.getElementById("leapYear1").innerHTML = isLeapYear(1986);
+document.getElementById("leapYear2").innerHTML = isLeapYear(2008);
+document.getElementById("leapYear3").innerHTML = isLeapYear(2000);
