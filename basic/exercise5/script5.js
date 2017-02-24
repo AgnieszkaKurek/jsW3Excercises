f6b334b3374f4200e590228bdf9c833917456bbe
AgnieszkaKurek <agniechakurek@gmail.com>
2017-02-24 16:09:21 +0100

    move console.log

--- basic/exercise5/script5.js
+++ basic/exercise5/script5.js
@@ -1,12 +1,14 @@
 'use strict';
 
-function firstSunday (startYear , endYear){
-    for (var year = startYear ; year <= endYear;  year++) {
+function firstSunday(startYear, endYear) {
+    for (var year = startYear; year <= endYear; year++) {
         var d = new Date(year, 0, 1);
-         if (d.getDay() === 0){
-           console.log(`The first day of ${year} is a Sunday.`);
-         };
+        if (d.getDay() === 0) {
+            return year;
+        };
     };
 };
 
+console.log(`The first day of ${year} is a Sunday`);
 firstSunday(2014, 2050);
+
