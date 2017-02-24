b7ce250adf6a20d20f1620d31d98ec35dee24a4b
AgnieszkaKurek <agniechakurek@gmail.com>
2017-02-24 10:01:44 +0100

    add function firstSunday (find 1st January is being a Sunday)

--- basic/exercise5/script5.js
+++ basic/exercise5/script5.js
@@ -1,1 +1,12 @@
 'use strict';
+
+function firstSunday() {
+    for (var year = 2014; year <= 2050; year++) {
+        var d = new Date(year, 0, 1);
+        if (d.getDay() === 0) {
+            document.getElementById("newYear").innerHTML = `The first day of ${year} is a Sunday.`
+        };
+    };
+};
+
+firstSunday();
