1a1a1f6098465cb2f0f85bb7a5d9cd1cdc9feadd
AgnieszkaKurek <agniechakurek@gmail.com>
2017-02-24 17:15:46 +0100

    format document and removal unnecessary letters

--- basic/exercise5/script5.js
+++ basic/exercise5/script5.js
@@ -4,10 +4,10 @@ function findFirstYearStartingFromSunday(startYear, endYear) {
     for (var year = startYear; year <= endYear; year++) {
         var d = new Date(year, 0, 1);
         if (d.getDay() === 0) {
-          return year;
+            return year;
         };
     };
 };
 
 var year = findFirstYearStartingFromSunday(2014, 2050);
-console.log(`The first day of ${year} is a Sunday`); 
+console.log(`The first day of ${year} is Sunday`); 
