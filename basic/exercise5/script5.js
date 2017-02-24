d8a678a25f8ed1aa5468213cfb3f290a2549547a
AgnieszkaKurek <agniechakurek@gmail.com>
2017-02-24 14:36:41 +0100

    rename  function parameter

--- basic/exercise5/script5.js
+++ basic/exercise5/script5.js
@@ -1,7 +1,7 @@
 'use strict';
 
-function firstSunday (yearX, yearY){
-    for (var year = yearX; year <= yearY;  year++) {
+function firstSunday (startYear , endYear){
+    for (var year = startYear ; year <= endYear;  year++) {
         var d = new Date(year, 0, 1);
          if (d.getDay() === 0){
            console.log(`The first day of ${year} is a Sunday.`);
