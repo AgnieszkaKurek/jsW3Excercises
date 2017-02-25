c26547474a698caafef686fd8b1c41741e029ef0
AgnieszkaKurek <agniechakurek@gmail.com>
2017-02-25 14:55:17 +0100

    add new var years

--- basic/exercise5/script5.js
+++ basic/exercise5/script5.js
@@ -1,13 +1,16 @@
 'use strict';
 
 function findFirstYearStartingFromSunday(startYear, endYear) {
+    var years = [];
+        years.push(year);
     for (var year = startYear; year <= endYear; year++) {
         var d = new Date(year, 0, 1);
         if (d.getDay() === 0) {
-            return year;
+           
         };
     };
+    return years;
 };
 
-var year = findFirstYearStartingFromSunday(2014, 2050);
-console.log(`The first day of ${year} is Sunday`); 
+var years = findFirstYearStartingFromSunday(2014, 2050);
+console.log(`The first day of ${years} is Sunday`); 
