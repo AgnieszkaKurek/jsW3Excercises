8d3a7c5bacddc9fde8271e33464b8b2b0556e5df
Agnieszka Kurek <Agnieszka Kurek>
2017-02-25 15:35:51 +0100

    rename function and replace push year to array years

--- basic/exercise5/script5.js
+++ basic/exercise5/script5.js
@@ -1,16 +1,15 @@
 'use strict';
 
-function findFirstYearStartingFromSunday(startYear, endYear) {
+function findAllYearStartingFromSunday(startYear, endYear) {
     var years = [];
-        years.push(year);
     for (var year = startYear; year <= endYear; year++) {
         var d = new Date(year, 0, 1);
         if (d.getDay() === 0) {
-           
+           years.push(year);
         };
     };
     return years;
 };
 
-var years = findFirstYearStartingFromSunday(2014, 2050);
+var years = findAllYearStartingFromSunday(2014, 2050);
 console.log(`The first day of ${years} is Sunday`); 
