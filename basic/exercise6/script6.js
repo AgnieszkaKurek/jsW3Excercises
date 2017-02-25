8a969836f5c66e3d0d618db39650f10e5a804cd6
AgnieszkaKurek <agniechakurek@gmail.com>
2017-02-25 14:41:23 +0100

    add function getNumber

--- basic/exercise6/script6.js
+++ basic/exercise6/script6.js
@@ -1,1 +1,13 @@
 'use strict';
+
+function getNumber(min, max) {
+    var number = Math.ceil(Math.random() * (max - min + 1)) + min;
+    var gnum = prompt('Guess the number between 1 and 10 inclusive');
+    if (gnum == number)
+        alert('Matched');
+    else
+        alert('Not matched, the number was ' + number);
+};
+var numericalInterval = getNumber(1, 10);
+console.log(numericalInterval);
+
