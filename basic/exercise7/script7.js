2f8f02d5484a5ba248aa4e8b48275db73637c31f
Agnieszka Kurek <Agnieszka Kurek>
2017-02-27 11:00:52 +0100

    add new function calculateDaysUntilToNextChristmas

--- basic/exercise7/script7.js
+++ basic/exercise7/script7.js
@@ -1,1 +1,18 @@
 'use strict';
+
+function calculateDaysUntilToNextChristmas() {
+
+    var oneDay = 1000 * 60 * 60 * 24;
+    var today = new Date;
+    var Christmas = new Date(today.getFullYear(), 11, 25);
+    if (today.getMonth() == 11 && today.getDate() > 25) {
+        Christmas.setFullYear(Christmas.getFullYear() + 1)
+    };
+    //var dateNextChristmas = Math.ceil(Christmas.getDate() - today.getDate());
+    var timeUntilChristmas = Math.ceil((Christmas.getTime()-today.getTime())/(oneDay));
+    return timeUntilChristmas;
+};
+
+var timeUntilChristmas = calculateDaysUntilToNextChristmas();
+console.log(`There are ${timeUntilChristmas} days to Christmas`);
+document.getElementById("timeToNextChristmas").innerHTML = `There are ${timeUntilChristmas} days to Christmas`;
