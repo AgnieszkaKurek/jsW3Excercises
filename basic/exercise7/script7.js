53290f7c8b8961b755c87083c47dda19a2f58b8d
AgnieszkaKurek <agniechakurek@gmail.com>
2017-02-28 15:13:46 +0100

    Feature 16 (#23)
    * add new function calculateDaysUntilToNextChristmas
    
    * remove unesesary element
    
    * rename function and variable, remove  empty line
    
    * correct Date object initiation
    
    * add new date to calculate days until to next Chrismas
    
    * add function  displayNumberOfDaysUntilNextChristmas
    
    * add string to function displayNumberOfDaysUntilNextChristmas
    
    * rename variable and foramt date
    
    * rename variable
    
    * rename variable

--- basic/exercise7/script7.js
+++ basic/exercise7/script7.js
@@ -1,1 +1,23 @@
 'use strict';
+
+function getNumberOfDaysUntilNextChristmas(date) {
+    var oneDayInMiliseconds = 1000 * 60 * 60 * 24;
+    var decemberIndex = 11;
+    var firstChristmasDays = 25;
+    var christmasDate = new Date(date.getFullYear(), decemberIndex, firstChristmasDays);
+    if (date.getMonth() === decemberIndex && date.getDate() > firstChristmasDays) {
+        christmasDate.setFullYear(christmasDate.getFullYear() + 1);
+    };
+    var timeUntilChristmas = Math.ceil((christmasDate.getTime() - date.getTime()) / oneDayInMiliseconds);
+    return timeUntilChristmas;
+};
+
+function displayNumberOfDaysUntilNextChristmas(date, id) {
+    var numberOfDaysUntilNextChristmas = getNumberOfDaysUntilNextChristmas(date);
+    var informationAboutNuberOfDaysFromDateToNextChrismas = `From ${date} is ${numberOfDaysUntilNextChristmas} days to next Christmas.`;
+    document.getElementById(id).innerHTML = informationAboutNuberOfDaysFromDateToNextChrismas;
+};
+
+displayNumberOfDaysUntilNextChristmas(new Date(), "timeToNextChristmas1");
+displayNumberOfDaysUntilNextChristmas(new Date(2017, 11, 1), "timeToNextChristmas2");
+displayNumberOfDaysUntilNextChristmas(new Date(2017, 11, 28), "timeToNextChristmas3");
