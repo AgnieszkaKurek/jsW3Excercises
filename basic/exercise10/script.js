3ccf39d1a0bdaefa812bcc088a32203f8487520f
Agnieszka Kurek <Agnieszka Kurek>
2017-03-01 14:23:50 +0100

    improve one name value

--- basic/exercise10/script.js
+++ basic/exercise10/script.js
@@ -8,6 +8,6 @@ function calculateMultiplication() {
 
 function calculateDivide() {
     var number1 = document.getElementById("number1").value;
-    var umber2 = document.getElementById("number2").value;
+    var number2 = document.getElementById("number2").value;
     document.getElementById("result").innerHTML = number1 / number2;
 };
