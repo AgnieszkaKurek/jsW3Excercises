079498d3dfe36a308ad0e1e9b25160bf120ef0f1
AgnieszkaKurek <agniechakurek@gmail.com>
2017-03-01 16:13:09 +0100

    add function  calculateMultiplication and calculateDivide and file st… (#25)
    * add function  calculateMultiplication and calculateDivide and file style.css
    
    * add var , format document
    
    * improve one name value
    
    * removing error id = result
    
    * add match.floor
    
    * Set the value of a input field
    
    * format document, add the missing apostrophe and remove unnecessary actions

--- basic/exercise10/script.js
+++ basic/exercise10/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+function calculateMultiplication() {
+    var number1 = document.getElementById("number1").value;
+    var number2 = document.getElementById("number2").value;
+    var result = document.getElementById("result");
+    result.value = number1 * number2;
+};
+
+function calculateDivide() {
+    var number1 = document.getElementById("number1").value;
+    var number2 = document.getElementById("number2").value;
+    var result = document.getElementById("result");
+    result.value = number1 / number2;
+};
