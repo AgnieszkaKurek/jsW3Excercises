405af29e7054639ae7170ccdd5aac17fbec2dcbe
Agnieszka Kurek <Agnieszka Kurek>
2017-03-01 12:36:47 +0100

    add function  calculateMultiplication and calculateDivide and file style.css

--- basic/exercise10/script.js
+++ basic/exercise10/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+function calculateMultiplication() {
+    Number1 = document.getElementById("Number1").value;
+    Number2 = document.getElementById("Number2").value;
+    document.getElementById("result").innerHTML = Number1 * Number2;
+};
+
+function calculateDivide() {
+    Number1 = document.getElementById("Number1").value;
+    Number2 = document.getElementById("Number2").value;
+    document.getElementById("result").innerHTML = Number1 / Number2;
+};
