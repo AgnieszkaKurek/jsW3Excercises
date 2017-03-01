48dd8c7e0c52c1615e09dff067df58f7141de917
Agnieszka Kurek <Agnieszka Kurek>
2017-03-01 16:07:37 +0100

    format document, add the missing apostrophe and remove unnecessary actions

--- basic/exercise10/script.js
+++ basic/exercise10/script.js
@@ -4,12 +4,12 @@ function calculateMultiplication() {
     var number1 = document.getElementById("number1").value;
     var number2 = document.getElementById("number2").value;
     var result = document.getElementById("result");
-    result.value = Math.floor(number1 * number2);
+    result.value = number1 * number2;
 };
 
 function calculateDivide() {
     var number1 = document.getElementById("number1").value;
     var number2 = document.getElementById("number2").value;
     var result = document.getElementById("result");
-    result.value = Math.floor(number1 / number2);
+    result.value = number1 / number2;
 };
