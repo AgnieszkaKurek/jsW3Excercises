54a6ff17d32eabf2f772aac465b307c6c16a108f
Agnieszka Kurek <Agnieszka Kurek>
2017-03-02 22:24:02 +0100

    add new functions: performCalculate, multiplay, divide

--- basic/exercise10/script.js
+++ basic/exercise10/script.js
@@ -1,19 +1,28 @@
 'use strict';
 
-function calculateMultiplication() {
+function performCalculate(calculate) {
     var number1 = document.getElementById("number1").value;
     var number2 = document.getElementById("number2").value;
     var result = document.getElementById("result");
-    result.value = number1 * number2;
+    result.value = calculate(number1, number2);
 };
 
-function calculateDivide() {
-    var number1 = document.getElementById("number1").value;
-    var number2 = document.getElementById("number2").value;
-    var result = document.getElementById("result");
-    result.value = number1 / number2;
+function multiplay(number1, number2) {
+    return number1 * number2;
+};
+
+function divide(number1, number2) {
     if (number2 == 0) {
-         var result = document.getElementById("result");
-        result.value = "Action illegal - you can not divide by 0!!!";
+        return "Action illegal - you can not divide by 0.";
+    } else {
+        return number1 / number2;
     }
+};
+
+function calculateMultiplication() {
+    performCalculate(multiplay);
+};
+
+function calculateDivision() {
+    performCalculate(divide)
 };
