f42f8dfba4a9c7a15d6366636fc31e516d681374
Agnieszka Kurek <agniechakurek@gmail.com>
2017-03-06 18:31:25 +0100

    changes  in unit test

--- basic/exercise11/scriptSpec.js
+++ basic/exercise11/scriptSpec.js
@@ -1,15 +1,15 @@
 /// <reference path="../../typings/globals/jasmine/index.d.ts" />
 
-describe("10 degrees Celsius" , function(){
-    it("is 50 degrees Fahrenheit", function(){
+describe(" Degrees Celsius" , function(){
+    it("is convert to degrees Fahrenheit", function(){
         var degreesFahrenheit =  convertTemperatureFromCelsiusToFahrenheit(10);
         expect(degreesFahrenheit).toEqual(50);
     });
 });
 
-describe("60 degrees Fahrenheit ", function(){
-    it("is 15.56 degrees Celsius", function(){
+describe(" Degrees Fahrenheit ", function(){
+    it("is convert to degrees Celsius", function(){
         var degreesCelsius =  convertTemperaturesFromFahrenheitToCelsius(60);
-        expect(degreesCelsius).toEqual(15,56);
+        expect(degreesCelsius).toEqual(15.56);
     });
 });
