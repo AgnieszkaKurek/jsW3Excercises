4f209082abc1156c7724198fa543536d1e16d81c
Agnieszka Kurek <agniechakurek@gmail.com>
2017-03-10 10:55:47 +0100

    add new test

--- basic/exercise11/scriptSpec.js
+++ basic/exercise11/scriptSpec.js
@@ -1,8 +1,6 @@
 /// <reference path="../../typings/globals/jasmine/index.d.ts" />
 /*globals convertTemperatureFromCelsiusToFahrenheit*/
 /*globals convertTemperatureFromFahrenheitToCelsius*/
-/*globals convertTemperatureBackToCelsius*/
-/*globals convertTemperatureBackToFahrenheit*/
 
 describe("Convert temperature from Celsius to Fahrenheit:", function () {
     it("0 degrees Celsius is 32 degrees Fahrenheit", function () {
@@ -32,26 +30,26 @@ describe("Convert temperature from Fahrenheit to Celsius:", function () {
 
 describe("Convert temperature beck from Fahrenheit to Celsius:", function () {
     it("beck temperature from 32 degrees Fahrenheit to 0 degrees Celsius", function () {
-        var backToDegreesCelsius = convertTemperatureBackToCelsius(32);
-        var fixedBackToDegeesCelsius = backToDegreesCelsius.toFixed(2);
-        expect(fixedBackToDegeesCelsius).toBe("0.00");
+        var degreesFahrenheit = convertTemperatureFromFahrenheitToCelsius(32);
+        var fixedBackToDegeesFahrenheit = degreesFahrenheit.toFixed(2);
+        expect(fixedBackToDegeesFahrenheit).toBe("0.00");
     });
     it("beck temperature from 50 degrees Fahrenheit to 10 degrees Celsius", function () {
-        var backToDegreesCelsius = convertTemperatureBackToCelsius(50);
-        var fixedBackToDegeesCelsius = backToDegreesCelsius.toFixed(2);
-        expect(fixedBackToDegeesCelsius).toBe("10.00");
+        var degreesFahrenheit = convertTemperatureFromFahrenheitToCelsius(50);
+        var fixedBackToDegreesFahrenheit = degreesFahrenheit.toFixed(2);
+        expect(fixedBackToDegreesFahrenheit).toBe("10.00");
     });
 });
 
 describe("Convert temperature beck from Celsius to Fahrenheit:", function () {
     it("back temperature from -17.78 degrees Celsius to 0 degrees Fahrenheit", function () {
-        var backToDegreesFahrenheit = convertTemperatureBackToFahrenheit(-17.78);
-        var fixedBackToDegeesFahrenheit = backToDegreesFahrenheit.toFixed(2);
-        expect(fixedBackToDegeesFahrenheit).toBe("0.00");
+        var degreesCelsius = convertTemperatureFromCelsiusToFahrenheit(-17.78);
+        var fixedBackToDegreesCelsius = degreesCelsius.toFixed(2);
+        expect(fixedBackToDegreesCelsius).toBe("0.00");
     });
     it("back temperature from 15.56 degrees Celsius to 0 degrees Fahrenheit", function () {
-        var backToDegreesFahrenheit = convertTemperatureBackToFahrenheit(15.56);
-        var fixedBackToDegeesFahrenheit = backToDegreesFahrenheit.toFixed(2);
-        expect(fixedBackToDegeesFahrenheit).toBe("60.00");
+        var degreesCelsius = convertTemperatureFromCelsiusToFahrenheit(15.56);
+        var fixedBackToDegeesCelsius = degreesCelsius.toFixed(2);
+        expect(fixedBackToDegeesCelsius).toBe("60.0");
     });
 });
