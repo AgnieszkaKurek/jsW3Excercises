e4dc9c0c9180c894abdabd804633ab674f1df77f
Agnieszka Kurek <agniechakurek@gmail.com>
2017-03-14 12:57:47 +0100

    add unit test to check if word  is palindrome

--- functions/exercise2/scriptSpec.js
+++ functions/exercise2/scriptSpec.js
@@ -1,1 +1,17 @@
 /// <reference path="../../typings/globals/jasmine/index.d.ts" />
+/*globals checkPalindrome*/
+
+describe("Check if word is palindrome", function () {
+    it("tenet read from the back is tenet", function () {
+        var wordFromTheRear = checkPalindrome("tenet");
+        expect(wordFromTheRear).toBe("tenet");
+       });
+       it("civic read from the back is civic", function () {
+        var wordFromTheRear = checkPalindrome("civic");
+        expect(wordFromTheRear).toBe("civic");
+       });
+       it("kayak read from the back is kayak", function () {
+        var wordFromTheRear = checkPalindrome("kayak");
+        expect(wordFromTheRear).toBe("kayak");
+       });
+}); 
