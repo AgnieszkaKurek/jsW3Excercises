ece1f0f70ba8f8e982a4f2cf85da885fb100240a
Agnieszka Kurek <agniechakurek@gmail.com>
2017-03-16 10:27:41 +0100

    improving the names and descriptions, add another case to test

--- functions/exercise2/scriptSpec.js
+++ functions/exercise2/scriptSpec.js
@@ -10,26 +10,30 @@ describe("Checking if word is palindrome: ", function () {
         var palindrome = isPalindrome("dog");
         expect(palindrome).toBeFalsy();
     });
-    it("if after palidromem is spacia 'kayak ', it is palidrom too", function () {
+    it("if after palidromem is space 'kayak ', it is palidrome too", function () {
         var palindrome = isPalindrome("kayak ");
         expect(palindrome).toBeTruthy();
     });
-    it("if before palidromem is spacia ' kayak', it is palidrom too", function () {
+    it("if before palindrome is space ' kayak', it is palidrome too", function () {
         var palindrome = isPalindrome(" kayak");
         expect(palindrome).toBeTruthy();
     });
-    it("if between two palindromes is spacia 'kayak kayak', it is a palindrome too", function () {
+    it("palindrome may contain white spaces, like in phrase 'kayak kayak'", function () {
         var palindrome = isPalindrome("kayak kayak");
         expect(palindrome).toBeTruthy();
+    });
+    it("phrase is composed of different palindromes separated white spaces, like 'kayak eye', is not palidrome", function () {
+        var palindrome = isPalindrome("kayak eye");
+        expect(palindrome).toBeFalsy();
     });
     it("One letter, 'a' is palindrome", function () {
         var palindrome = isPalindrome("a");
         expect(palindrome).toBeTruthy();
     });
-    it("no letters, '' is not a palindrome", function () {
-        expect(function () { isPalindrome(""); }).toThrow("Parameter must contain at least one letter");
+    it("no letters, '' is exception", function () {
+        expect(function () { isPalindrome(""); }).toThrow("Parameter must contain at least one letter - not use white space");
     });
-    it("space, ' ' is not a palidrom", function () {
-        expect(function () { isPalindrome(" "); }).toThrow("Parameter must contain at least one letter");
+    it("space, ' ' is exception", function () {
+        expect(function () { isPalindrome(" "); }).toThrow("Parameter must contain at least one letter - not use white space");
     });
 });
