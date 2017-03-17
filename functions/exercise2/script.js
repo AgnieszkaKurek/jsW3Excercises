3dce6a293c855186d017ec24475d4baddc43315f
AgnieszkaKurek <agniechakurek@gmail.com>
2017-03-17 07:58:24 +0100

    Feature 32 (#39)
    * implemetafunctions implemented
    
    * functions implemented

--- functions/exercise2/script.js
+++ functions/exercise2/script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+function isPalindrome(phrase) {
+    var normalizedPhrase = phrase.toLowerCase().trim();
+    if (normalizedPhrase === "") {
+        throw new Error("Parameter must contain at least one letter - not use white space");
+    }
+    var reversedNormalizedPhrase = normalizedPhrase.split("").reverse().join("");
+    return reversedNormalizedPhrase === normalizedPhrase;
+}
