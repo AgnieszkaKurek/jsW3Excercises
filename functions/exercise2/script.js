22f6892331afccd01e1fd59c63313a3c95c6658a
Agnieszka Kurek <agniechakurek@gmail.com>
2017-03-17 07:44:50 +0100

    implemetafunctions implemented

--- functions/exercise2/script.js
+++ functions/exercise2/script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+function isPalindrome(phrase) {
+var result1 = phrase.toLowerCase().trim();
+var  result = result1.split("").reverse().join("");
+if (result === "") {
+        throw new Error("Parameter must contain at least one letter - not use white space");
+    }
+    
+return result === result1;
+}
