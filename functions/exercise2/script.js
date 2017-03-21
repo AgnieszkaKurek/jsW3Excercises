72e902db4db6c1e1479cfa110322c8696586aaa8
Agnieszka Kurek <agniechakurek@gmail.com>
2017-03-21 16:50:45 +0100

    removal input, add output , correct function showResult()

--- functions/exercise2/script.js
+++ functions/exercise2/script.js
@@ -3,18 +3,20 @@
 function isPalindrome(phrase) {
     var normalizedPhrase = phrase.toLowerCase().trim();
     if (normalizedPhrase === "") {
-        throw new Error("Parameter must contain at least one letter - not use white space");
+        throw new Error("Parameter must contain at least one letter - not use white space!!!");
     }
     var reversedNormalizedPhrase = normalizedPhrase.split("").reverse().join("");
-    return reversedNormalizedPhrase === normalizedPhrase;
+    var result = reversedNormalizedPhrase === normalizedPhrase;
+    if (result === true) { result = "is a palindrome!"; }
+    else { result = "isn't a palindrome!"; }
+    return result;
 }
 
 function showResult() {
     var phrase = document.getElementById("phrase").value;
     try {
         var result = isPalindrome(phrase);
-        if (result === true) { document.getElementById("result").value = `${phrase} is palindrome :)`; }
-        if (result === false) { document.getElementById("result").value = `${phrase} isn't palindrome :(`; }
+        document.getElementById("result").value = `${phrase} ${result}`;
     }
     catch (exception) {
         document.getElementById("result").value = exception;
