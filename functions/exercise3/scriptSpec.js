cac4524824365b24fa5bd2e18b2a798999ec49a1
Agnieszka Kurek <agniechakurek@gmail.com>
2017-03-23 20:09:47 +0100

    getAllSubStringsOf tests

--- functions/exercise3/scriptSpec.js
+++ functions/exercise3/scriptSpec.js
@@ -1,21 +1,29 @@
 /// <reference path="../../typings/globals/jasmine/index.d.ts" />
-/*globals getAllCombinationsOfLetters*/
+/*globals getAllSubStringsOf*/
 
-describe("With a letters, located in string", function () {
-    it("'dog'can generated 6 Combination", function () {
-        var result = getAllCombinationsOfLetters("dog");
-        expect(result).toContain("d", "do", "dog", "o","og","g");
+describe("Getting all substrings:", function () {
+    it("empty phrase has 0 substrings", function () {
+        var substrings = getAllSubStringsOf("");
+        expect(substrings).toEqual([]);
     });
-     it("'house'can generated 6 Combination", function () {
-        var result = getAllCombinationsOfLetters("house");
-        expect(result).toContain("h", "ho", "hou", "hous", "house", "ou", "ous", "use", "ouse", "se", "e");
+    it("white spaces are treated as standard characters", function () {
+        var substrings = getAllSubStringsOf(" ");
+        expect(substrings).toEqual([" "]);
     });
-    it("''can generated 6 Combination", function () {
-        var result = getAllCombinationsOfLetters("dog");
-        expect(result).toContain("");
+    it("single character has 1 substring", function () {
+        var substrings = getAllSubStringsOf("a");
+        expect(substrings).toEqual(["a"]);
     });
-    it("' 'can generated 6 Combination", function () {
-        var result = getAllCombinationsOfLetters("dog");
-        expect(result).toContain(" ");
+    it("two characters have 3 substrings", function () {
+        var substrings = getAllSubStringsOf("ab");
+        expect(substrings).toEqual(["a", "ab", "b"]);
+    }); 
+    it("three characters have 6 substrings", function () {
+        var substrings = getAllSubStringsOf("dog");
+        expect(substrings).toEqual("d", "do","dog","o","og","g");
+    });
+    it("four characters have 10 substrings", function () {
+        var substrings = getAllSubStringsOf("home");
+        expect(substrings).toEqual("h","ho","hom","home","o","om","ome","m","me","e");
     });
 });
