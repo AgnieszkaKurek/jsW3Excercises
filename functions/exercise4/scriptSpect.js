eb703d8edd7073f11bea17cc2fa1cffa12e4308c
Agnieszka Kurek <agniechakurek@gmail.com>
2017-03-28 15:48:15 +0200

    add new unnit test

--- functions/exercise4/scriptSpect.js
+++ functions/exercise4/scriptSpect.js
@@ -1,37 +1,8 @@
 /// <reference path="../../typings/globals/jasmine/index.d.ts" />
-/*globals returnStringAlphabeticalOrder */
+/*globals validatePhrase*/
 
-describe("Checking if phrase is  returned in alphabetical order: ", function () {
-    it("phrase 'b' returned is a 'no'", function () {
-        var result = returnStringAlphabeticalOrder("b");
-        expect(result).toBeTruthy();
-    });
-    it("phrase 'on' returned is a 'no'", function () {
-        var result = returnStringAlphabeticalOrder("no");
-        expect(result).toBeTruthy();
-    });
-    it("phrase 'pig' returned is a 'gip'", function () {
-        var result = returnStringAlphabeticalOrder("gip");
-        expect(result).toBeTruthy();
-    });
-    it("phrase 'definitively' returned is a 'deefiiilntvy'", function () {
-        var result = returnStringAlphabeticalOrder("deefiiilntvy");
-        expect(result).toBeTruthy();
-    });
-
-    it("no letters, '' is an exception", function () {
-        expect(function () { returnStringAlphabeticalOrder(""); }).toThrow(new Error("Parameter must contain at least two letter - not use white space"));
-    });
-    it("space, ' ' is an exception", function () {
-        expect(function () { returnStringAlphabeticalOrder(" "); }).toThrow(new Error("Parameter must contain at least two letter - not use white space"));
-    });
-    it("space, '  ' is an exception", function () {
-        expect(function () { returnStringAlphabeticalOrder("  "); }).toThrow(new Error("Parameter must contain at least two letter - not use white space"));
-    });
-    it("space, '12' is an exception", function () {
-        expect(function () { returnStringAlphabeticalOrder("12"); }).toThrow(new Error("Parameter must contain at least two letter - not use number"));
-    });
-    it("space, '!' is an exception", function () {
-        expect(function () { returnStringAlphabeticalOrder("!"); }).toThrow(new Error("Parameter must contain at least two letter - not use Assume punctuation"));
-    });
+describe("Check if", function () {
+  it("phrase is number ", function () {
+    expect(function () { validatePhrase(Number); }).toThrow(new Error("Phrase can't be number"));
+  });
 });
