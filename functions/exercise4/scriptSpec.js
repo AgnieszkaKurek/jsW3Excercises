5cf5267240695d39b42400368719699f64a399f0
Agnieszka Kurek <agniechakurek@gmail.com>
2017-03-29 13:14:52 +0200

    Add dots at the end of the sentence

--- functions/exercise4/scriptSpec.js
+++ functions/exercise4/scriptSpec.js
@@ -6,12 +6,12 @@ describe("Check if phrase consists of letters only.", function () {
     expect(function () { validateAsPhraseConsistsOfLettersOnly(" "); }).toThrow(new Error("The parameter should be consist only of letters."));
   });
   it("number 1 is an exception", function () {
-    expect(function () { validateAsPhraseConsistsOfLettersOnly(1); }).toThrow(new Error("The parameter should be consist only of letters"));
+    expect(function () { validateAsPhraseConsistsOfLettersOnly(1); }).toThrow(new Error("The parameter should be consist only of letters."));
   });
   it("number '1' is an exception", function () {
-    expect(function () { validateAsPhraseConsistsOfLettersOnly("1"); }).toThrow(new Error("The parameter should be consist only of letters"));
+    expect(function () { validateAsPhraseConsistsOfLettersOnly("1"); }).toThrow(new Error("The parameter should be consist only of letters."));
   });
   it("punctuation mark '!' is an exception", function () {
-    expect(function () { validateAsPhraseConsistsOfLettersOnly("!"); }).toThrow(new Error("The parameter should be consist only of letters"));
+    expect(function () { validateAsPhraseConsistsOfLettersOnly("!"); }).toThrow(new Error("The parameter should be consist only of letters."));
   });
 });
