d488ea2beb97633c0dbaa6eecb1a7e06b98f144e
Agnieszka Kurek <agniechakurek@gmail.com>
2017-03-30 21:50:02 +0200

    rename function

--- functions/exercise4/script.js
+++ functions/exercise4/script.js
@@ -1,10 +1,9 @@
 "use strict";
 
 function validateAsPhraseConsistsOfLettersOnly(phrase) {
-    var isvalid = /^[a-zA-Z]*$/g.test(phrase);
-    if (!isvalid) {
+    var isValid = /^[a-zA-Z]*$/g.test(phrase);
+    if (!isValid) {
         throw new Error("The parameter should be consist only of letters.");
     }
     return true;
 }
-
