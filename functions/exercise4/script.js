304c977be73dd3caf1b6c8c18e73d7a770c2e619
Agnieszka Kurek <agniechakurek@gmail.com>
2017-03-30 18:25:16 +0200

    add validate function

--- functions/exercise4/script.js
+++ functions/exercise4/script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+function validateAsPhraseConsistsOfLettersOnly(phrase) {
+    if (/^[a-zA-Z]*$/g.test(phrase)) {
+        return phrase;
+    }
+    if (!/^[a-zA-Z]*$/g.test(phrase)) {
+        throw new Error("The parameter should be consist only of letters.");
+    }
+}
