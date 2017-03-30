d12bdbce576920cf0f63222b149086539f223f42
Agnieszka Kurek <agniechakurek@gmail.com>
2017-03-30 19:03:38 +0200

    Code reduction

--- functions/exercise4/script.js
+++ functions/exercise4/script.js
@@ -2,10 +2,8 @@
 
 function validateAsPhraseConsistsOfLettersOnly(phrase) {
     var validate = /^[a-zA-Z]*$/g.test(phrase);
-    if (validate) {
-        return true;
-    }
     if (!(validate)) {
         throw new Error("The parameter should be consist only of letters.");
     }
+    return true;
 }
