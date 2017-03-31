4e4510f6e60c8631a3aaa652b25a897f31b4c773
Agnieszka Kurek <agniechakurek@gmail.com>
2017-03-31 12:29:49 +0200

    Correct spelling mistakes and adding correct arguments

--- functions/exercise5/scriptSpect.js
+++ functions/exercise5/scriptSpect.js
@@ -1,20 +1,20 @@
 /// <reference path="../../typings/globals/jasmine/index.d.ts" />
-/*globals convertWordFirstLetterInUpperCase*/
-describe("Converted the first letter of word of the string in upper case:", function () {
+/*globals convertWordFirstLetterIntoUpperCase*/
+describe("Converting the first letter of each word in the string into upper case", function () {
   it("empty string '' gives empty string ''", function () {
-    var stringInUpperCase = convertWordFirstLetterInUpperCase("");
+    var stringInUpperCase = convertWordFirstLetterIntoUpperCase("");
     expect(stringInUpperCase).toEqual("");
   });
   it("string 'c' gives  string 'C'", function () {
-    var stringInUpperCase = convertWordFirstLetterInUpperCase("");
+    var stringInUpperCase = convertWordFirstLetterIntoUpperCase("c");
     expect(stringInUpperCase).toEqual("C");
   });
   it("string 'piglet' gives  string 'Piglet'", function () {
-    var stringInUpperCase = convertWordFirstLetterInUpperCase("");
+    var stringInUpperCase = convertWordFirstLetterIntoUpperCase("piglet");
     expect(stringInUpperCase).toEqual("Piglet");
   });
    it("string 'the quick brown fox' gives  string 'The Quick Brown Fox'", function () {
-    var stringInUpperCase = convertWordFirstLetterInUpperCase("");
+    var stringInUpperCase = convertWordFirstLetterIntoUpperCase("the quick brown fox");
     expect(stringInUpperCase).toEqual("The Quick Brown Fox");
   });
 }); 
