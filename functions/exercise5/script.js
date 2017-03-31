61aa3944f43709cf41b5f94540c7e161e980fd11
Agnieszka Kurek <agniechakurek@gmail.com>
2017-03-31 18:06:09 +0200

    foramat document

--- functions/exercise5/script.js
+++ functions/exercise5/script.js
@@ -1,13 +1,11 @@
 "use strict";
 
-function convertWordFirstLetterIntoUpperCase(string){
+function convertWordFirstLetterIntoUpperCase(string) {
     var splitString = string.split(" ");
     var array = [];
-    for(var i = 0; i < splitString.length; i++){
+    for (var i = 0; i < splitString.length; i++) {
         array.push(splitString[i].charAt(0).toUpperCase() + splitString[i].slice(1));
     }
     var stringInUpperCase = array.join(" ");
-   return stringInUpperCase;
+    return stringInUpperCase;
 }
-
-
