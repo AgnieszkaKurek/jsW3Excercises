6e63ae02a8ac71f3a82a34417a5c52a6e78ad9dd
Agnieszka Kurek <agniechakurek@gmail.com>
2017-04-06 00:14:36 +0200

    add unit test and implementation  function cheking if number is prime

---
+++ functions/exercise8/scriptSpec.js
@@ -0,0 +1,29 @@
+/// <reference path="../../typings/globals/jasmine/index.d.ts" />
+/*globals isPrimeNumber*/
+
+describe("Checking if number is prime:", function () {
+    it("number '3' is a prime", function () {
+        var testNumber = isPrimeNumber(3);
+        expect(testNumber).toBeTruthy();
+    });
+    it("number '4' isn't a prime", function () {
+        var testNumber = isPrimeNumber(4);
+        expect(testNumber).toBeFalsy();
+    });
+    it("number '547' is a prime", function () {
+        var result = isPrimeNumber(547);
+        expect(result).toBeTruthy();
+    });
+    it("number '666' isn't a prime", function () {
+        var result = isPrimeNumber(666);
+        expect(result).toBeFalsy();
+    });
+    it("number '15485053' is a prime", function () {
+        var result = isPrimeNumber(15485053);
+        expect(result).toBeTruthy();
+    });
+    it("number '77777777' isn't a prime", function () {
+        var result = isPrimeNumber(77777777);
+        expect(result).toBeFalsy();
+    });
+}); 
