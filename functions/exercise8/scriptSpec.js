6c24f730174a18b62f3d493f4f80dc64b3c078ef
Agnieszka Kurek <agniechakurek@gmail.com>
2017-04-06 14:56:18 +0200

    changes toBeTruthy and toBeFalsy to toBe(true) and toBe(false)

--- functions/exercise8/scriptSpec.js
+++ functions/exercise8/scriptSpec.js
@@ -4,34 +4,34 @@
 describe("Checking if number is prime:", function () {
     it("number '1' isn' a prime", function () {
         var testNumber = isPrimeNumber(1);
-        expect(testNumber).toBeFalsy();
+        expect(testNumber).toBe(false);
     });
      it("number '2' is a prime", function () {
         var testNumber = isPrimeNumber(2);
-        expect(testNumber).toBeTruthy();
+        expect(testNumber).toBe(true);
     });
     it("number '3' is a prime", function () {
         var testNumber = isPrimeNumber(3);
-        expect(testNumber).toBeTruthy();
+        expect(testNumber).toBe(true);
     });
     it("number '4' isn't a prime", function () {
         var testNumber = isPrimeNumber(4);
-        expect(testNumber).toBeFalsy();
+        expect(testNumber).toBe(false);
     });
     it("number '547' is a prime", function () {
         var result = isPrimeNumber(547);
-        expect(result).toBeTruthy();
+        expect(result).toBe(true);
     });
     it("number '666' isn't a prime", function () {
         var result = isPrimeNumber(666);
-        expect(result).toBeFalsy();
+        expect(result).toBe(false);
     });
     it("number '15485053' is a prime", function () {
         var result = isPrimeNumber(15485053);
-        expect(result).toBeTruthy();
+        expect(result).toBe(true);
     });
     it("number '77777777' isn't a prime", function () {
         var result = isPrimeNumber(77777777);
-        expect(result).toBeFalsy();
+        expect(result).toBe(false);
     });
 }); 
