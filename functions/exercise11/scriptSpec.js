75a90d54a96787707e6ccc9e263c6ce691eebced
Agnieszka Kurek <agniechakurek@gmail.com>
2017-04-20 11:51:07 +0200

    remove empty space

--- functions/exercise11/scriptSpec.js
+++ functions/exercise11/scriptSpec.js
@@ -4,6 +4,6 @@
 describe("Getting second lowest and second gratest numbers with array", function () {
     it("numerical sequence [1, 2, 3, 4, 5] get second lowest number 2 and second grates number 4", function () {
         var secondLowestAndGreatestNumbers = getSecondLowestAndSecondGreatestNumbers([1, 2, 3, 4, 5]);
-        expect(secondLowestAndGreatestNumbers).toEqual(2, 4);
+        expect(secondLowestAndGreatestNumbers).toEqual(2,4);
     });
 });
