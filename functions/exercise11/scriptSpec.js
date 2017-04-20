70cccb079c2c4585fa5c3a55b0e7b056321c0166
Agnieszka Kurek <agniechakurek@gmail.com>
2017-04-20 13:52:55 +0200

    rename value and add new exaples

--- functions/exercise11/scriptSpec.js
+++ functions/exercise11/scriptSpec.js
@@ -3,8 +3,18 @@
 
 describe("Getting second lowest and second gratest numbers with array", function () {
     it("numerical sequence [1, 2, 3, 4, 5] get second lowest number 2 and second grates number 4", function () {
-        var number =  getSecondLowestAndSecondGreatestNumbers([1, 2, 3, 4, 5]);
-        expect(number.SecondSmallest).toEqual(2);
-        expect(number.SecondLargest).toEqual(4);
+        var numbers =  getSecondLowestAndSecondGreatestNumbers([1, 2, 3, 4, 5]);
+        expect(numbers.SecondSmallest).toEqual(2);
+        expect(numbers.SecondLargest).toEqual(4);
+    });
+   it("numerical sequence [1,9] get second lowest number 9 and second grates number 1", function () {
+        var numbers =  getSecondLowestAndSecondGreatestNumbers([1,9]);
+        expect(numbers.SecondSmallest).toEqual(9);
+        expect(numbers.SecondLargest).toEqual(1);
+    });
+    it("numerical sequence [13] get second lowest number 13 and second grates number 13", function () {
+        var numbers =  getSecondLowestAndSecondGreatestNumbers([13]);
+        expect(numbers.SecondSmallest).toEqual(13);
+        expect(numbers.SecondLargest).toEqual(13);
     });
 });
