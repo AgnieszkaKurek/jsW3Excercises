fef499cb351ef4460ce051a4eb42ea251754f525
Agnieszka Kurek <agniechakurek@gmail.com>
2017-04-20 12:06:04 +0200

    improving unit test and add new  examples

--- functions/exercise11/scriptSpec.js
+++ functions/exercise11/scriptSpec.js
@@ -3,7 +3,20 @@
 
 describe("Getting second lowest and second gratest numbers with array", function () {
     it("numerical sequence [1, 2, 3, 4, 5] get second lowest number 2 and second grates number 4", function () {
-        var secondLowestAndGreatestNumbers = getSecondLowestAndSecondGreatestNumbers([1, 2, 3, 4, 5]);
-        expect(secondLowestAndGreatestNumbers).toEqual(2,4);
+        var secondLowestNumbers = getSecondLowestAndSecondGreatestNumbers([1, 2, 3, 4, 5]);
+        var secondGreatestNumbers = getSecondLowestAndSecondGreatestNumbers([1, 2, 3, 4, 5]);
+        expect(secondLowestNumbers).toEqual(2);
+        expect(secondGreatestNumbers).toEqual(4);
     });
+     it("numerical sequence [2, 4, 6, 8] get second lowest number 2 and second grates number 4", function () {
+       var secondLowestNumbers = getSecondLowestAndSecondGreatestNumbers([2, 4, 6, 8]);
+        var secondGreatestNumbers = getSecondLowestAndSecondGreatestNumbers([2, 4, 6, 8]);
+        expect(secondLowestNumbers).toEqual(4);
+        expect(secondGreatestNumbers).toEqual(8);
+    });
+     it("numerical sequence [1,2] get second lowest number 2 and second grates number 4", function () {
+       var secondLowestNumbers = getSecondLowestAndSecondGreatestNumbers([1, 2]);
+        var secondGreatestNumbers = getSecondLowestAndSecondGreatestNumbers([1, 2]);
+        expect(secondLowestNumbers).toEqual(2);
+        expect(secondGreatestNumbers).toEqual(1);
 });
