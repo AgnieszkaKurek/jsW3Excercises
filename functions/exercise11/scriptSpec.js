1905d365a9e8efa4f4e063d191c777fd3ca8cf5d
AgnieszkaKurek <agniechakurek@gmail.com>
2017-04-21 07:30:44 +0200

    Feature57 (#63)
    * add new folder
    
    * add  unit test
    
    * rename parameter
    
    * rename variable and error correction
    
    * remove empty space
    
    * improving unit test and add new  examples
    
    * correcting  unit test
    
    * rename value and add new exaples
    
    * add new example
    
    * correcting examples

---
+++ functions/exercise11/scriptSpec.js
@@ -0,0 +1,30 @@
+/// <reference path="../../typings/globals/jasmine/index.d.ts" />
+/*globals getSecondLowestAndSecondGreatestNumbers*/
+
+describe("Getting second lowest and second gratest numbers with array", function () {
+    it("numerical sequence [1, 2, 3, 4, 5] get second lowest number 2 and second grates number 4", function () {
+        var numbers =  getSecondLowestAndSecondGreatestNumbers([1, 2, 3, 4, 5]);
+        expect(numbers.SecondSmallest).toEqual(2);
+        expect(numbers.SecondLargest).toEqual(4);
+    });
+   it("numerical sequence [1,9] get second lowest number 9 and second grates number 1", function () {
+        var numbers =  getSecondLowestAndSecondGreatestNumbers([1,9]);
+        expect(numbers.SecondSmallest).toEqual(9);
+        expect(numbers.SecondLargest).toEqual(1);
+    });
+    it("numerical sequence [13] second lowest number is undefined  and second grates undefined", function () {
+        var numbers =  getSecondLowestAndSecondGreatestNumbers([13]);
+        expect(numbers.SecondSmallest).toEqual(undefined);
+        expect(numbers.SecondLargest).toEqual(undefined);
+    });
+    it("numerical sequence [4,1,2,5,3] get second lowest number 4 and second grates number 2 - sorting does not matter", function () {
+        var numbers =  getSecondLowestAndSecondGreatestNumbers([4,1,2,5,3]);
+        expect(numbers.SecondSmallest).toEqual(2);
+        expect(numbers.SecondLargest).toEqual(4);
+    });
+    it("numerical sequence [1,1,2,3,3] get second lowest number 2 and second grates number 2", function () {
+        var numbers =  getSecondLowestAndSecondGreatestNumbers([1,1,2,3,3]);
+        expect(numbers.SecondSmallest).toEqual(2);
+        expect(numbers.SecondLargest).toEqual(2);
+    });
+});
