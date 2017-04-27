133d9154444c6149fbe3df01b70e9fb9464e56a1
Agnieszka Kurek <agniechakurek@gmail.com>
2017-04-27 08:45:31 +0200

    correction of function code

--- functions/exercise11/script.js
+++ functions/exercise11/script.js
@@ -1,18 +1,30 @@
 "use strict";
 
 function getSecondLowestAndSecondGreatestNumbers(numericalSequence) {
-    var smallestNumber;
+    var smallest;
     var secondSmallest;
-    var largestNumber;
+    var largest;
     var secondLargest;
     for (var i = 0; i < numericalSequence.length; i++) {
         var current = numericalSequence[i];
-        if (largestNumber === undefined) { largestNumber = current; }
-        if (largestNumber != undefined && largestNumber < current) { largestNumber = secondLargest; largestNumber = current; }
-        if (largestNumber > current && secondLargest === undefined || secondLargest < current) { current = secondLargest; }
-        if (smallestNumber === undefined) { smallestNumber = current; }
-        if (smallestNumber < current) { smallestNumber = secondSmallest; smallestNumber = current; }
-        if (smallestNumber > current && secondSmallest === undefined || secondSmallest < current) { secondSmallest = current; }
+
+        if (largest === undefined) {
+            largest = current;
+        } else if (current > largest) {
+            secondLargest = largest;
+            largest = current;
+        } else if ((current < largest) && (secondLargest === undefined || secondLargest < current)) {
+            secondLargest = current;
+        }
+
+        if (smallest === undefined) {
+            smallest = current;
+        } else if (current < smallest) {
+            secondSmallest = smallest;
+            smallest = current;
+        } else if ((current > smallest) && (secondSmallest === undefined || secondSmallest > current)) {
+            secondSmallest = current;
+        }
     }
 
     return {
