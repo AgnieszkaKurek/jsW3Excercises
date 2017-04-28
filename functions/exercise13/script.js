baf138e9bfedc0690938062e298de5210617449d
Agnieszka Kurek <agniechakurek@gmail.com>
2017-04-28 12:39:56 +0200

    add new case

---
+++ functions/exercise13/script.js
@@ -0,0 +1,5 @@
+"use strict";
+
+function  getFactorsOfPositiveInteger(number) {
+   
+}
--- functions/exercise13/scriptSpec.js
+++ functions/exercise13/scriptSpec.js
@@ -3,12 +3,24 @@
 
 
 describe("Getting factors of a positive integer", function () {
-    it("factor numbers 1 is 1", function () {
+    it("factors of number 1 are 1", function () {
         var factors = getFactorsOfPositiveInteger(1);
-        expect(factors).toEqual(1);
+        expect(factors).toEqual([1]);
     });
-    it("factors numbers 10 are 1,2,5,10", function () {
+    it("factors of number X are are 1,2,5,10", function () {
         var factors = getFactorsOfPositiveInteger(10);
         expect(factors).toEqual([1,2,5,10]);
+    });
+    it(" factors of number 12 are 1,3,4,12", function () {
+        var factors = getFactorsOfPositiveInteger(12);
+        expect(factors).toEqual([1,2,5,10]);
+    });
+     it("factors of number 25 are are 1,5,25", function () {
+        var factors = getFactorsOfPositiveInteger(25);
+        expect(factors).toEqual([1,2,5,10]);
+    });
+    it("factors of number 100 are 1, 2, 4, 5, 10, 20, 25, 50, 100", function () {
+        var factors = getFactorsOfPositiveInteger(100);
+        expect(factors).toEqual([1,2,4,5,10,20,25,50,100]);
     });
 });
