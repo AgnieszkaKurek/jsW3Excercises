15e298ab48a6a1c4d6bceac3e9d6f30766350fad
Agnieszka Kurek <agniechakurek@gmail.com>
2017-05-04 14:36:09 +0200

    File formatting

--- functions/exercise13/script.js
+++ functions/exercise13/script.js
@@ -3,7 +3,8 @@
 function getFactorsOfPositiveInteger(checkedNumber) {
     var factorsNumbers = [];
     for (var i = 1; i <= checkedNumber; i++)
-        if (checkedNumber % i === 0)
-        { factorsNumbers.push(i);}
+        if (checkedNumber % i === 0) {
+            factorsNumbers.push(i);
+        }
     return factorsNumbers;
 }
--- functions/exercise13/scriptSpec.js
+++ functions/exercise13/scriptSpec.js
@@ -9,18 +9,18 @@ describe("Getting factors of a positive integer", function () {
     });
     it("factors of number 10 are 1,2,5,10", function () {
         var factors = getFactorsOfPositiveInteger(10);
-        expect(factors).toEqual([1,2,5,10]);
+        expect(factors).toEqual([1, 2, 5, 10]);
     });
     it(" factors of number 12 are 1,2,3,4,6,12", function () {
         var factors = getFactorsOfPositiveInteger(12);
-        expect(factors).toEqual([1,2,3,4,6,12]);
+        expect(factors).toEqual([1, 2, 3, 4, 6, 12]);
     });
-     it("factors of number 25 are are 1,5,25", function () {
+    it("factors of number 25 are are 1,5,25", function () {
         var factors = getFactorsOfPositiveInteger(25);
-        expect(factors).toEqual([1,5,25]);
+        expect(factors).toEqual([1, 5, 25]);
     });
     it("factors of number 100 are 1, 2, 4, 5, 10, 20, 25, 50, 100", function () {
         var factors = getFactorsOfPositiveInteger(100);
-        expect(factors).toEqual([1,2,4,5,10,20,25,50,100]);
+        expect(factors).toEqual([1, 2, 4, 5, 10, 20, 25, 50, 100]);
     });
 });
