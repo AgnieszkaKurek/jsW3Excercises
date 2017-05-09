c60ff64385fec7cc35958972254f9a40e8b80ff3
Agnieszka Kurek <agniechakurek@gmail.com>
2017-05-09 10:32:10 +0200

    remove the error and empty spaces

--- functions/exercise14/scriptSpec.js
+++ functions/exercise14/scriptSpec.js
@@ -3,11 +3,11 @@
 
 describe("Getting  amount of coins ", function () {
     it("in the amount of 5 are 5", function () {
-        var amountOfCoins = getAmountOfCoins (5);
+        var amountOfCoins = getAmountOfCoins(5);
         expect(amountOfCoins).toEqual([5]);
     });
-    it("in the amount of 45 are 25, 10, 10, 1", function () {
-        var amountOfCoins = getAmountOfCoins (45);
+    it("in the amount of 46 are 25, 10, 10, 1", function () {
+        var amountOfCoins = getAmountOfCoins(46);
         expect(amountOfCoins).toEqual([25, 10, 10, 1]);
     });
 });
