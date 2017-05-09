97d5a495fc8ec51af641257aca7e87baa4b94688
AgnieszkaKurek <agniechakurek@gmail.com>
2017-05-09 11:21:24 +0200

    add new folder and files , add unit test (#67)
    * add new folder and files , add unit test
    
    * remove the error and empty spaces

---
+++ functions/exercise14/scriptSpec.js
@@ -0,0 +1,13 @@
+/// <reference path="../../typings/globals/jasmine/index.d.ts" />
+/*globals getAmountOfCoins*/
+
+describe("Getting  amount of coins ", function () {
+    it("in the amount of 5 are 5", function () {
+        var amountOfCoins = getAmountOfCoins(5);
+        expect(amountOfCoins).toEqual([5]);
+    });
+    it("in the amount of 46 are 25, 10, 10, 1", function () {
+        var amountOfCoins = getAmountOfCoins(46);
+        expect(amountOfCoins).toEqual([25, 10, 10, 1]);
+    });
+});
