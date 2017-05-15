2ed07b70378286aa7cfa395e2fb073cf6f9eb70f
Agnieszka Kurek <agniechakurek@gmail.com>
2017-05-15 09:47:17 +0200

    rename variable

--- functions/exercise14/script.js
+++ functions/exercise14/script.js
@@ -1,11 +1,11 @@
 "use strict";
 
 function getAmountOfCoins(amount) {
-    var nominals = [25, 10, 5, 2, 1];
+    var denominations = [25, 10, 5, 2, 1];
     var coin = [];
     var curentCoinIndex = 0;
     while (amount > 0) {
-        var curentCoin = nominals[curentCoinIndex];
+        var curentCoin = denominations[curentCoinIndex];
         while (amount >= curentCoin) {
             coin.push(curentCoin);
             amount -= curentCoin;
