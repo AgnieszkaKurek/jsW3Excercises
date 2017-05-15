1a9227129ecc27d277e561c01acbd41d1cd3781c
Agnieszka Kurek <agniechakurek@gmail.com>
2017-05-15 12:02:54 +0200

    simplify code

--- functions/exercise15/script.js
+++ functions/exercise15/script.js
@@ -1,6 +1,5 @@
 "use strict";
 
-function exponentiation(baseOfPower, exponent){
-var result = Math.pow(baseOfPower, exponent);
-return result;
+function exponentiation(baseOfPower, exponent) {
+    return Math.pow(baseOfPower, exponent);
 }
