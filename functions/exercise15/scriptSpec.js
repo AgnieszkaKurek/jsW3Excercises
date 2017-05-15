5c8a98dbf4bf3ce300b0010689d49be6f692af9a
Agnieszka Kurek <agniechakurek@gmail.com>
2017-05-15 11:30:59 +0200

    add unit test

--- functions/exercise15/scriptSpec.js
+++ functions/exercise15/scriptSpec.js
@@ -1,2 +1,10 @@
 /// <reference path="../../typings/globals/jasmine/index.d.ts" />
-/*globals */
+/*globals exponentiation*/
+
+describe("Exponentiation of number", function () {
+     it("1 raised to the power of 0  is 0", function () {
+        var result = exponentiation(1,0);
+        expect(result).toEqual(0);
+    });
+   
+  });
