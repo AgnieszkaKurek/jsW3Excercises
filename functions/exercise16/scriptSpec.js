fa1a466b37f217d3ac0f27c080988ae58925786a
Agnieszka Kurek <agniechakurek@gmail.com>
2017-05-15 13:52:48 +0200

    removal typo

--- functions/exercise16/scriptSpec.js
+++ functions/exercise16/scriptSpec.js
@@ -1,20 +1,20 @@
 /// <reference path="../../typings/globals/jasmine/index.d.ts" />
 /*globals getUniqueCharactersFromString*/
 
-describe("Extract unique character from a string", function () {
-    it("string 'pig' give 'pig' uniqe character", function () {
+describe("Extract unique characters from a string", function () {
+    it("string 'pig' give 'pig' uniqe characters", function () {
         var uniqueCharacters = getUniqueCharactersFromString("pig");
         expect(uniqueCharacters).toEqual("pig");
     });
-    it("string 'dodo' give 'do' uniqe character", function () {
+    it("string 'dodo' give 'do' uniqe characters", function () {
         var uniqueCharacters = getUniqueCharactersFromString("dodo");
         expect(uniqueCharacters).toEqual("do");
     });
-    it("string 'future' give 'futre' uniqe character", function () {
+    it("string 'future' give 'futre' uniqe characters", function () {
         var uniqueCharacters = getUniqueCharactersFromString("future");
         expect(uniqueCharacters).toEqual("futre");
     });
-    it("string 'thequickbrownfoxjumpsoverthelazydog' give 'thequickbrownfxjmpsvlazydg'uniqe character", function () {
+    it("string 'thequickbrownfoxjumpsoverthelazydog' give 'thequickbrownfxjmpsvlazydg'uniqe characters", function () {
         var uniqueCharacters = getUniqueCharactersFromString("thequickbrownfoxjumpsoverthelazydog");
         expect(uniqueCharacters).toEqual("thequickbrownfxjmpsvlazydg");
     });
