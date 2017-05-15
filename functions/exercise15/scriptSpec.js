beaaa60b9193818f12323abde7db88d4cdd4a9cb
Agnieszka Kurek <agniechakurek@gmail.com>
2017-05-15 11:49:56 +0200

    Correcting calculation error and add new examples to unit test

--- functions/exercise15/scriptSpec.js
+++ functions/exercise15/scriptSpec.js
@@ -2,9 +2,20 @@
 /*globals exponentiation*/
 
 describe("Exponentiation of number", function () {
-     it("1 raised to the power of 0  is 0", function () {
-        var result = exponentiation(1,0);
-        expect(result).toEqual(0);
+    it("1 raised to the power of 0  is 1", function () {
+        var result = exponentiation(1, 0);
+        expect(result).toEqual(1);
     });
-   
-  });
+    it("-2 raised to the power of 1  is -2", function () {
+        var result = exponentiation(-2, 1);
+        expect(result).toEqual(-2);
+    });
+    it("2 raised to the power of 3 is 8 ", function () {
+        var result = exponentiation(2, 3);
+        expect(result).toEqual(8);
+    });
+    it("5 raised to the power of 5 is 3125 ", function () {
+        var result = exponentiation(5, 5);
+        expect(result).toEqual(3125);
+    });
+});
