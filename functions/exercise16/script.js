b25de86b94a3f21bb45f521de2fd6a3a89082444
Agnieszka Kurek <agniechakurek@gmail.com>
2017-05-15 15:34:41 +0200

    add function getUniqueCharactersFromString

--- functions/exercise16/script.js
+++ functions/exercise16/script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+function getUniqueCharactersFromString(string) {
+    var uniqueCharacters = "";
+    for (var i = 0; i < string.length; i++) {
+        if (uniqueCharacters.indexOf(string.charAt(i)) == -1) {
+            uniqueCharacters += string[i];
+        }
+    }
+    return uniqueCharacters;
+}
