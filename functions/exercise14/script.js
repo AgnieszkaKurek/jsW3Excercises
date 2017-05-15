441d751aae1cde4d0d013266e2c373a83c5957d9
Agnieszka Kurek <agniechakurek@gmail.com>
2017-05-15 09:35:27 +0200

    remove typo

--- functions/exercise14/script.js
+++ functions/exercise14/script.js
@@ -1,11 +1,11 @@
 "use strict";
 
 function getAmountOfCoins(amount) {
-    var nominasls = [25, 10, 5, 2, 1];
+    var nominals = [25, 10, 5, 2, 1];
     var coin = [];
     var curentCoinIndex = 0;
     while (amount > 0) {
-        var curentCoin = nominasls[curentCoinIndex];
+        var curentCoin = nominals[curentCoinIndex];
         while (amount >= curentCoin) {
             coin.push(curentCoin);
             amount -= curentCoin;
