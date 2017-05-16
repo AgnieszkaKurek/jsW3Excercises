fe73b455dab1e07efc7193a8ceb8e69da2b5330e
Agnieszka Kurek <agniechakurek@gmail.com>
2017-05-16 09:56:00 +0200

    code unification

--- functions/exercise16/script.js
+++ functions/exercise16/script.js
@@ -1,11 +1,12 @@
 "use strict";
 
-function getUniqueCharactersFromString(string) {
-    var uniqueCharacters = "";
-    for (var i = 0; i < string.length; i++) {
-        if (uniqueCharacters.indexOf(string.charAt(i)) == -1) {
-            uniqueCharacters += string[i];
+function getUniqueCharactersFromString(phrase) {
+    var uniqueChars = "";
+    for (var i = 0; i < phrase.length; i++) {
+        var currentChar = phrase[i];
+        if (uniqueChars.indexOf(currentChar) == -1) {
+            uniqueChars += currentChar;
         }
     }
-    return uniqueCharacters;
+    return uniqueChars;
 }
