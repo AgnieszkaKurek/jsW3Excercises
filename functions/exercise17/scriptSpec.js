04cabe98ff16edbd58c34376a66b8c777fe42fdb
Agnieszka Kurek <agniechakurek@gmail.com>
2017-05-17 22:33:45 +0200

    improvement unit test

--- functions/exercise17/scriptSpec.js
+++ functions/exercise17/scriptSpec.js
@@ -1,14 +1,48 @@
 /// <reference path="../../typings/globals/jasmine/index.d.ts" />
-/*globals getNumberOfOccurrencesOfLetters*/
+/*globals getQuantityOfLetters*/
 
-describe("Getting the number of occurrences of each letters in specified phrase", function () {
-    it("phrase 'a' gives one letter 'a'", function () {
-        var numbersOfLetters = getNumberOfOccurrencesOfLetters(["a"]);
-        expect(numbersOfLetters.letter.a).toEqual([1]);
+describe("Getting the quantity of occurrences of each letter in a phrase", function () {
+    it("phrase 'a' gives one letter", function () {
+        var quantityOfLetters = getQuantityOfLetters("a");
+        expect(quantityOfLetters).toEqual([
+            { letter: "a", quantity: 1 },
+        ]);
+    });
+    it("phrase 'Aa' gives two letter 'a'", function () {
+        var quantityOfLetters = getQuantityOfLetters("a");
+        expect(quantityOfLetters).toEqual([
+            { letter: "a", quantity: 2 },
+        ]);
+    });
+    it("in the phrase 'pig' each letter occurs once", function () {
+        var quantityOfLetters = getQuantityOfLetters("a");
+        expect(quantityOfLetters).toEqual([
+            { letter: "p", quantity: 1 },
+            { letter: "i", quantity: 1 },
+            { letter: "g", quantity: 1 },
+        ]);
+    });
+    it("in the phrase 'piggip' each letter occurs twice", function () {
+        var quantityOfLetters = getQuantityOfLetters("a");
+        expect(quantityOfLetters).toEqual([
+            { letter: "p", quantity: 2 },
+            { letter: "i", quantity: 2 },
+            { letter: "g", quantity: 2 },
+        ]);
+    });
+    it("in the phrase 'pigs and piglets' letter 'p','i','g','s' occurs twice and  the rest of the letters occurs once", function () {
+        var quantityOfLetters = getQuantityOfLetters("a");
+        expect(quantityOfLetters).toEqual([
+            { letter: "p", quantity: 2 },
+            { letter: "i", quantity: 2 },
+            { letter: "g", quantity: 2 },
+            { letter: "s", quantity: 2 },
+            { letter: "a", quantity: 1 },
+            { letter: "n", quantity: 1 },
+            { letter: "d", quantity: 1 },
+            { letter: "l", quantity: 1 },
+            { letter: "e", quantity: 1 },
+            { letter: "t", quantity: 1 },
+        ]);
     });
 });
-/// object
-/// var numbersOfLetters = {
-///    "letter": "a",
-///    "quantity": 1
-/// };
