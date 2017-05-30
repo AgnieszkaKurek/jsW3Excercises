7090f86d742cb5d60dd15746c9c179f061dc53fe
Agnieszka Kurek <agniechakurek@gmail.com>
2017-05-30 10:41:42 +0200

    add corect implementation

--- functions/exercise17/script.js
+++ functions/exercise17/script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+function getQuantityOfLetters(phrase) {
+    var quantityOfLetters = [];
+    //todo: change for i into different for loop from ES6
+    for (var i = 0; i < phrase.length; i++) {
+        var character = phrase[i].toLowerCase();
+        if (character.match(/[a-z]/)) {
+            var existingChars = quantityOfLetters.filter(item => item.letter === character);
+            if (existingChars.length === 0) {
+                quantityOfLetters.push({ letter: character, quantity: 1 });
+            } else {
+                existingChars[0].quantity++;
+            }
+        }
+    }
+    return quantityOfLetters;
+}
