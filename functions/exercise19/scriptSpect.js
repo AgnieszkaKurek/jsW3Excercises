fd2e5e3b105bc321764d2a8f9cef190f601342ee
Agnieszka Kurek <agniechakurek@gmail.com>
2017-05-31 09:56:45 +0200

    formating file

--- functions/exercise19/scriptSpect.js
+++ functions/exercise19/scriptSpect.js
@@ -2,25 +2,25 @@
 /*globals getElementsLargerThanNumber*/
 
 describe("Returning array elements larger than a number", function () {
-     it("empty array aren't any elements larger than 10", function () {
-        var elementsLargerThanANumber = getElementsLargerThanNumber(10,[]);
+    it("empty array aren't any elements larger than 10", function () {
+        var elementsLargerThanANumber = getElementsLargerThanNumber(10, []);
         expect(elementsLargerThanANumber).toEqual([]);
     });
     it("in the array whit white space [ ]  aren't any elements larger than 10", function () {
-        var elementsLargerThanANumber = getElementsLargerThanNumber(10,[ ]);
+        var elementsLargerThanANumber = getElementsLargerThanNumber(10, []);
         expect(elementsLargerThanANumber).toEqual([]);
     });
-     it("in the array [a] aren't any elements larger than 10", function () {
-        var elementsLargerThanANumber = getElementsLargerThanNumber(10,["a"]);
+    it("in the array [a] aren't any elements larger than 10", function () {
+        var elementsLargerThanANumber = getElementsLargerThanNumber(10, ["a"]);
         expect(elementsLargerThanANumber).toEqual([]);
     });
-     it("in the array [10] aren't any elements larger than 10", function () {
-        var elementsLargerThanANumber = getElementsLargerThanNumber(10,[10]);
+    it("in the array [10] aren't any elements larger than 10", function () {
+        var elementsLargerThanANumber = getElementsLargerThanNumber(10, [10]);
         expect(elementsLargerThanANumber).toEqual([]);
     });
-    
+
     it("in the array [2, 5, 10] aren't any elements larger than 10 ", function () {
-        var elementsLargerThanANumber = getElementsLargerThanNumber(10,[2, 5, 10]);
+        var elementsLargerThanANumber = getElementsLargerThanNumber(10, [2, 5, 10]);
         expect(elementsLargerThanANumber).toEqual([]);
     });
     it("in the array [2, 5, 10, 15, 20, 30] elements larger than 10 are [15, 20, 30]", function () {
