108754762e0f7b1ac6f6b1eb23cffb1371926107
Agnieszka Kurek <agniechakurek@gmail.com>
2017-05-31 07:37:19 +0200

    correcting the test

--- functions/exercise19/scriptSpect.js
+++ functions/exercise19/scriptSpect.js
@@ -1,9 +1,9 @@
 /// <reference path="../../typings/globals/jasmine/index.d.ts" />
-/*globals getElementLargerThanANumber*/
+/*globals getElementLargerThanAnumber*/
 
-describe("Getting elements larger than a number.", function () {
-    it("in numbers array 2, 5, 10, 15, 20, 30 larger than 10 are 15, 20, 30  ", function () {
-        var elementLargerThanANumber = getElementLargerThanANumber(10);
+describe("Returning array elements larger than a number", function () {
+    it("in the array [2, 5, 10, 15, 20, 30] elements larger than 10 are [15, 20, 30]", function () {
+        var elementLargerThanANumber = getElementLargerThanAnumber([2, 5, 10, 15, 20, 30]);
         expect(elementLargerThanANumber).toEqual([15, 20, 30]);
     });
 });
