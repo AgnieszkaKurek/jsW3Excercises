e7fa899d472f41c615bb49c4c69db24955e40005
Agnieszka Kurek <agniechakurek@gmail.com>
2017-05-31 11:59:08 +0200

    Add new examples and improvement  existing

--- functions/exercise19/scriptSpec.js
+++ functions/exercise19/scriptSpec.js
@@ -10,9 +10,11 @@ describe("Returning array elements larger than a number", function () {
         var elementsLargerThanANumber = returnElementsLargerThanNumber(10, []);
         expect(elementsLargerThanANumber).toEqual([]);
     });
-    it("in the array [a] aren't any elements larger than 10", function () {
-        var elementsLargerThanANumber = returnElementsLargerThanNumber(10, ["a"]);
-        expect(elementsLargerThanANumber).toEqual([]);
+    it("white space [ ] is an exception ", function () {
+        expect(function () { returnElementsLargerThanNumber(" "); }).toThrow(new Error("Parameter must contain at least one number - not use white space"));
+    });
+    it("letter is an exception ", function () {
+        expect(function () { returnElementsLargerThanNumber("a"); }).toThrow(new Error("Parameter must contain at least one number - not use letters"));
     });
     it("in the array [10] aren't any elements larger than 10", function () {
         var elementsLargerThanANumber = returnElementsLargerThanNumber(10, [10]);
@@ -23,8 +25,20 @@ describe("Returning array elements larger than a number", function () {
         var elementsLargerThanANumber = returnElementsLargerThanNumber(10, [2, 5, 10]);
         expect(elementsLargerThanANumber).toEqual([]);
     });
+    it("in the array [-2, -5, 10, 15] elements larger than 10 are [15]", function () {
+        var elementsLargerThanNumber = returnElementsLargerThanNumber(10, [-2, -5, 10, 15]);
+        expect(elementsLargerThanNumber).toEqual([15]);
+    });
     it("in the array [2, 5, 10, 15, 20, 30] elements larger than 10 are [15, 20, 30]", function () {
         var elementsLargerThanNumber = returnElementsLargerThanNumber(10, [2, 5, 10, 15, 20, 30]);
         expect(elementsLargerThanNumber).toEqual([15, 20, 30]);
+    });
+    it("in the array [20, 30, 99 ,120] elements larger than 10 are [20, 30, 99 ,120]", function () {
+        var elementsLargerThanNumber = returnElementsLargerThanNumber(10, [20, 30, 99 ,120]);
+        expect(elementsLargerThanNumber).toEqual([20, 30, 99 ,120]);
+    });
+    it("in the array [13, 2, 250, 1, 3, 99] elements larger than 10 are [13, 250, 99]", function () {
+        var elementsLargerThanNumber = returnElementsLargerThanNumber(10, [13, 2, 250, 1, 3, 99]);
+        expect(elementsLargerThanNumber).toEqual([13, 250, 99]);
     });
 });
