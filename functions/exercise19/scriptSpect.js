2a1a864ef5feeaea0080e4d8e39c0389d258c1ee
Agnieszka Kurek <agniechakurek@gmail.com>
2017-05-31 08:43:59 +0200

    Remove errors and formating file

--- functions/exercise19/scriptSpect.js
+++ functions/exercise19/scriptSpect.js
@@ -3,7 +3,7 @@
 
 describe("Returning array elements larger than a number", function () {
     it("in the array [2, 5, 10, 15, 20, 30] elements larger than 10 are [15, 20, 30]", function () {
-        var elementLargerThanANumber = getElementLargerThanNumber(10,[2, 5, 10, 15, 20, 30]);
-        expect(elementLargerThanANumber).toEqual([15, 20, 30]);
+        var elementsLargerThanNumber = getElementLargerThanNumber(10, [2, 5, 10, 15, 20, 30]);
+        expect(elementsLargerThanNumber).toEqual([15, 20, 30]);
     });
 });
