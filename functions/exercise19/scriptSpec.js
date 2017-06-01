fedafc26b7bc93a32947f9b02c6e7d58cb6e4c37
Agnieszka Kurek <agniechakurek@gmail.com>
2017-06-01 12:48:35 +0200

    change description

--- functions/exercise19/scriptSpec.js
+++ functions/exercise19/scriptSpec.js
@@ -2,42 +2,45 @@
 /*globals returnElementsLargerThanNumber*/
 
 describe("Returning array elements larger than a number", function () {
-    it("empty array aren't any elements larger than 10", function () {
+    it("empty array aren't any elements  larger than referenc number", function () {
         var elementsLargerThanANumber = returnElementsLargerThanNumber(10, []);
         expect(elementsLargerThanANumber).toEqual([]);
     });
-    it("in the array whit white space [ ]  aren't any elements larger than 10", function () {
-        var elementsLargerThanANumber = returnElementsLargerThanNumber(10, []);
-        expect(elementsLargerThanANumber).toEqual([]);
+    
+    it("array can have numeric values only - in the array are only items different than numeric value", function () {
+        expect(function () { returnElementsLargerThanNumber("a"); }).toThrow(new Error("Array can have numeric values only."));
     });
-    it("white space [ ] is an exception ", function () {
-        expect(function () { returnElementsLargerThanNumber(" "); }).toThrow(new Error("Parameter must contain at least one number - not use white space"));
+
+    it("array can have numeric values only - in the array are also items different than numeric value", function () {
+        expect(function () { returnElementsLargerThanNumber("ab", 12, 10); }).toThrow(new Error("Array can have numeric values only."));
     });
-    it("letter is an exception ", function () {
-        expect(function () { returnElementsLargerThanNumber("a"); }).toThrow(new Error("Parameter must contain at least one number - not use letters"));
-    });
-    it("in the array [10] aren't any elements larger than 10", function () {
+
+    it("if in the array is only  referenc number, any elements are larger than referenc number ", function () {
         var elementsLargerThanANumber = returnElementsLargerThanNumber(10, [10]);
         expect(elementsLargerThanANumber).toEqual([]);
     });
 
-    it("in the array [2, 5, 10] aren't any elements larger than 10 ", function () {
+    it("if in the array there are only numbers smaller than the reference number, all items are not larger", function () {
         var elementsLargerThanANumber = returnElementsLargerThanNumber(10, [2, 5, 10]);
         expect(elementsLargerThanANumber).toEqual([]);
     });
-    it("in the array [-2, -5, 10, 15] elements larger than 10 are [15]", function () {
+
+    it("if in the array are negative values (they are also important here)  will be returned items larger than referenc number", function () {
         var elementsLargerThanNumber = returnElementsLargerThanNumber(10, [-2, -5, 10, 15]);
         expect(elementsLargerThanNumber).toEqual([15]);
     });
-    it("in the array [2, 5, 10, 15, 20, 30] elements larger than 10 are [15, 20, 30]", function () {
+
+    it("if in the array there are number smaller, larger  and the same like referens number, will be returned items larger", function () {
         var elementsLargerThanNumber = returnElementsLargerThanNumber(10, [2, 5, 10, 15, 20, 30]);
         expect(elementsLargerThanNumber).toEqual([15, 20, 30]);
     });
-    it("in the array [20, 30, 99 ,120] elements larger than 10 are [20, 30, 99 ,120]", function () {
+
+    it("if in the array there are only numbers larger than the reference number, all items are larger", function () {
         var elementsLargerThanNumber = returnElementsLargerThanNumber(10, [20, 30, 99 ,120]);
         expect(elementsLargerThanNumber).toEqual([20, 30, 99 ,120]);
     });
-    it("in the array [13, 2, 250, 1, 3, 99] elements larger than 10 are [13, 250, 99]", function () {
+    
+    it("if array does not to be sorted, thought will be returned items larger than referenc number", function () {
         var elementsLargerThanNumber = returnElementsLargerThanNumber(10, [13, 2, 250, 1, 3, 99]);
         expect(elementsLargerThanNumber).toEqual([13, 250, 99]);
     });
