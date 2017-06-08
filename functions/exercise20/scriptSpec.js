a97674a7f7b2f06ff643c2b337acd104e6e4f3a3
Agnieszka Kurek <agniechakurek@gmail.com>
2017-06-08 15:22:46 +0200

    corecting unit test

--- functions/exercise20/scriptSpec.js
+++ functions/exercise20/scriptSpec.js
@@ -1,13 +1,13 @@
 /// <reference path="../../typings/globals/jasmine/index.d.ts" />
 /*globals getStringIdOfRandomChars*/
 
-describe("Getting the a string id (specified length) of random characters", function () {
+describe("Getting the a string (specified length) of random characters", function () {
     it("If the specified length is 1, will be return one random character", function () {
-        var stringIdOfRandomChars = getStringIdOfRandomChars();
-        expect(stringIdOfRandomChars).toEqual(String,1);
+        var lengthExpectString = getStringIdOfRandomChars(1);
+         expect(lengthExpectString).toEqual(1);
     });
     it("If the specified length is 25, will be return 25 random characters", function () {
-        var stringIdOfRandomChars = getStringIdOfRandomChars(25);
-        expect(stringIdOfRandomChars).toEqual(String,25);
+        var lengthExpectString = getStringIdOfRandomChars(25);
+        expect(lengthExpectString).toEqual(25);
     });
 });
