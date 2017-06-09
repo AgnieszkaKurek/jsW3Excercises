da7ce0e445cdbd0bd5ae42b4556959dc752218b1
Agnieszka Kurek <agniechakurek@gmail.com>
2017-06-09 08:42:52 +0200

    add implementation

---
+++ functions/exercise20/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function getStringIdOfRandomChars(specifiedLength) {
+    const stringCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let getStringIdOfRandomChars = "";
+    for (let i = 0; i < specifiedLength; i++) {
+        getStringIdOfRandomChars += stringCharacters.charAt(Math.floor(Math.random() * stringCharacters.length));
+    }
+    return getStringIdOfRandomChars;
+}
