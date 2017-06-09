2ff0dcbdf1edeb13d31c258510628527c25d3aa8
Agnieszka Kurek <agniechakurek@gmail.com>
2017-06-09 13:03:08 +0200

    Small changes in code readability

--- functions/exercise20/script.js
+++ functions/exercise20/script.js
@@ -4,9 +4,13 @@ function getStringIdOfRandomChars(specifiedLength) {
     const stringCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     let getStringIdOfRandomChars = "";
     for (let i = 0; i < specifiedLength; i++) {
+        let stringCharactersLenght = stringCharacters.length;
         getStringIdOfRandomChars +=
-            stringCharacters[(Math.floor(Math.random()
-                * stringCharacters.length))];
+            stringCharacters
+            [(Math.floor
+                (Math.random()
+                * stringCharactersLenght))];
     }
     return getStringIdOfRandomChars;
+
 }
