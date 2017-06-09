589de5a89529d7f3ef44b5e0be41ac528a122455
Agnieszka Kurek <agniechakurek@gmail.com>
2017-06-09 14:46:25 +0200

    Deletion of typos

--- functions/exercise20/script.js
+++ functions/exercise20/script.js
@@ -2,10 +2,10 @@
 
 function getStringIdOfRandomChars(specifiedLength) {
     const stringCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    const stringCharactersLenght = stringCharacters.length;
+    const stringCharactersLength = stringCharacters.length;
     let randomCharacters = "";
     for (let i = 0; i < specifiedLength; i++) {
-        let randomIndex = Math.floor(Math.random() * stringCharactersLenght);
+        let randomIndex = Math.floor(Math.random() * stringCharactersLength);
         randomCharacters += stringCharacters[randomIndex];
     }
     return randomCharacters;
