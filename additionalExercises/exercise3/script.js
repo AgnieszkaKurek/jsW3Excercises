de0377395f1ab9ac6403f6e35e8ae104185ff3e4
Agnieszka Kurek <agniechakurek@gmail.com>
2017-07-07 08:38:30 +0200

    add new  folder and files , do very easy exersise

---
+++ additionalExercises/exercise3/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let scoops = 5;
+
+while (scoops>0) {
+    document.write("Another ice cream ball!!!");
+    scoops =  scoops-1;
+}
+document.write("Life without ice cream would be terrible!!!");
