16f842a885ce0fd000f06f8a5342584a435cbc6d
Agnieszka Kurek <agniechakurek@gmail.com>
2017-08-17 19:33:07 +0200

    add arrow function

--- functions/exercise21/scriptSpec.js
+++ functions/exercise21/scriptSpec.js
@@ -2,12 +2,12 @@
 /*globals getPossibleSubset*/
 
 describe("Getting all possible subset with a fixed length combinations in an array", (() => {
-    it("If the array [1, 2, 3] we will get all subsets of length 2, we get subsets [2, 1], [3, 1], [3, 2]", function () {
+    it("If the array [1, 2, 3] we will get all subsets of length 2, we get subsets [2, 1], [3, 1], [3, 2]",  (() => {
         var subsets = getPossibleSubset([1, 2, 3], 2);
         expect(subsets).toHaveSameItems([
             [3, 2],
             [3, 1],
             [2, 1]
         ]);
-    });
+    }));
 }));
