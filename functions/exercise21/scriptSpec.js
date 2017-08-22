36aba294b83b696e9f70cd43be7872e249318eef
Agnieszka Kurek <agniechakurek@gmail.com>
2017-08-22 21:55:23 +0200

    Improved performance of unit tests

--- functions/exercise21/scriptSpec.js
+++ functions/exercise21/scriptSpec.js
@@ -2,16 +2,12 @@
 /*globals getPossibleSubset*/
 
 describe("Getting all possible subset with a fixed length combinations in an array", (() => {
-    it("If the array [1, 2, 3] we will get all subsets of length 2, we get subsets [2, 1], [3, 1], [3, 2]",  (() => {
-        var subsets = getPossibleSubset([1, 2, 3], 2);
-        expect(subsets.elementOfsubset).toEqual([
-            [3, 2],
-        ]);
-        expect(subsets.elementOfsubset).toEqual([
-            [3, 1],
-        ]);
-        expect(subsets.elementOfsubset).toEqual([
-            [2, 1]
-        ]);
+    it("If the array [1, 2, 3] we will get all subsets of length 2, we get subsets [2, 1], [3, 1], [3, 2]", (() => {
+        //var subsets = getPossibleSubset([1, 2, 3], 2);
+        var subsets = [[3, 2], [2, 1], [3, 1]];
+        expect(subsets.length).toEqual(3);
+        expect(subsets.filter(x => x.length === 2 && x.includes(2) && x.includes(3)).length).toBeTruthy();
+        expect(subsets.filter(x => x.length === 2 && x.includes(2) && x.includes(1)).length).toBeTruthy();
+        expect(subsets.filter(x => x.length === 2 && x.includes(3) && x.includes(1)).length).toBeTruthy();
     }));
 }));
