5b5cd8fd5f11c8828c4ed4cec31d489e2b1b662d
Agnieszka Kurek <agniechakurek@gmail.com>
2017-08-24 16:52:21 +0200

    Change the names to more appropriate, adding missing pieces of code and formatting

--- functions/exercise21/script.js
+++ functions/exercise21/script.js
@@ -1,13 +1,17 @@
 "use strict";
 
-function getPossibleSubset(set, numberOfItems) {
+function getPossibleSubsets(set, numberOfItems) {
     if (numberOfItems === 2) {
-        let subset = [];
-        for (let i = 0; i < set.length; ++i)
-            for (let j = i + 1; j < set.length; ++j)
-                subset.push([set[i], set[j]]);
-        return subset;
+        let subsets = [];
+        for (let i = 0; i < set.length; ++i) {
+            for (let j = i + 1; j < set.length; ++j) {
+                subsets.push([set[i], set[j]]);
+            }
+        }
+        return subsets;
     } else {
         throw new Error("Number of subset different than 2 not implemented yet.");
     }
 }
+
+
--- functions/exercise21/scriptSpec.js
+++ functions/exercise21/scriptSpec.js
@@ -1,17 +1,17 @@
 /// <reference path="../../typings/globals/jasmine/index.d.ts" />
-/*globals getPossibleSubset*/
+/*globals getPossibleSubsets*/
 /*globals arrayOfArraysHaveTheSameItems*/
 
 describe("Getting all possible subset with a fixed length combinations in an array", (() => {
     it("If the array [1, 2, 3] we will get all subsets of length 2, we get subsets [2, 1], [3, 1], [3, 2]", (() => {
-        let subsets = getPossibleSubset([1, 2, 3], 2);
+        let subsets = getPossibleSubsets([1, 2, 3], 2);
         let expetedArray = [[3, 2], [2, 1], [3, 1]];
         let arraysAreEquvalent = arrayOfArraysHaveTheSameItems(subsets, expetedArray);
         expect(arraysAreEquvalent).toBe(true);
     }));
 
     it("If the array [1, 2, 3, 4] we will get all subsets of length 2, we get subsets [[1,2],[3,4],[1,3],[2,4],[1,4],[2,3]]", (() => {
-        let subsets = getPossibleSubset([1, 2, 3, 4], 2);
+        let subsets = getPossibleSubsets([1, 2, 3, 4], 2);
         let expetedArray = [[1,2],[3,4],[1,3],[2,4],[1,4],[2,3]];
         let arraysAreEquvalent = arrayOfArraysHaveTheSameItems(subsets, expetedArray);
         expect(arraysAreEquvalent).toBe(true);
