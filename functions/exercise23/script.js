99012da7ad347a952e9a7d5d58f8013e54e9f0a2
AgnieszkaKurek <agniechakurek@gmail.com>
2017-08-28 20:50:08 +0200

    delete the last change

--- functions/exercise23/script.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-function findFirstNotRepeatedCharacter(phrase) {
-    for (let item of phrase){
-        if(phrase.indexOf(item)===phrase.lastIndexOf(item)){
-            return item;
-        }
-    }
-}
