3b58a4dd543bb28ab42f65dc56f597075959c257
AgnieszkaKurek <agniechakurek@gmail.com>
2017-08-28 15:16:31 +0200

    Change in function countNumberOccurrencesOfSpecifiedLetterWithinString (use regular expression)

--- functions/exercise22/script.js
+++ functions/exercise22/script.js
@@ -1,11 +1,7 @@
 "use strict";
 
-function countNumberOccurrencesOfSpecifiedLetterWithinString(phrase, checkedLetter){
-   let countCheckedLetter = 0;
-   for(let i = 0; i < phrase.length; i++) {
-       if (phrase.charAt(i)== checkedLetter){
-           countCheckedLetter += 1;
-       }
-   }
-   return countCheckedLetter;
+function countNumberOccurrencesOfSpecifiedLetterWithinString(phrase, checkedLetter) {
+    let countCheckedLetter = phrase.match(RegExp(checkedLetter, "gi"));
+    let result = countCheckedLetter.length;
+    return result;
 }
