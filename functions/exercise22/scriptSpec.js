0a69e7faed4776b425749e5601bce05d20197b29
Agnieszka Kurek <agniechakurek@gmail.com>
2017-08-28 09:01:54 +0200

    Correcting the test description and remove empty lines

--- functions/exercise22/scriptSpec.js
+++ functions/exercise22/scriptSpec.js
@@ -1,19 +1,16 @@
 /// <reference path="../../typings/globals/jasmine/index.d.ts" />
 /*globals countNumberOccurrencesOfSpecifiedLetterWithinString*/
 
-
 describe("Count the number of occurrences of the specified letter within the string", (() => {
 
-    it("In the string the given letter occurs x times", (() => {
+    it("In the string 'wresourcecom' letter 'o' occurs 2 time", (() => {
         let numberOccurrencesLetter = countNumberOccurrencesOfSpecifiedLetterWithinString("wresourcecom", "o");
         expect(numberOccurrencesLetter).toEqual(2);
     }));
 
-    it("In the string the given letter occurs x times", (() => {
+    it("In the string 'I like fruit waffles and whipped cream' letter 'f' occurs 3 times", (() => {
         let numberOccurrencesLetter = countNumberOccurrencesOfSpecifiedLetterWithinString("I like fruit waffles and whipped cream", "f");
         expect(numberOccurrencesLetter).toEqual(3);
     }));
 
 }));
-
-
