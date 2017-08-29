440f5ceefd4d01321fbe374fdffb5a82d3b76665
AgnieszkaKurek <agniechakurek@gmail.com>
2017-08-29 14:07:00 +0200

    add function  find  not repeat  Character (#89)
    * add function  find  not repeat  Character
    
    * Add a test that fails while it should not
    
    * streamlined functions
    
    * Remove not needed variable

---
+++ functions/exercise23/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function findFirstNotRepeatedCharacter(phrase) {
+    for (let i = 0; i < phrase.length; i++) {
+        let char = phrase[i];
+        if (phrase.indexOf(char) === phrase.lastIndexOf(char)) {
+            return char;
+        }
+    }
+    return "";
+}
