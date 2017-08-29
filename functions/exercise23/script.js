b62c1aa2e4f3a21af9f19c755619d4006fe9e1e8
Krzysiek102 <krzysztof.kurek102@gmail.com>
2017-08-29 13:00:40 +0100

    Remove not needed variable

--- functions/exercise23/script.js
+++ functions/exercise23/script.js
@@ -1,13 +1,11 @@
 "use strict";
 
 function findFirstNotRepeatedCharacter(phrase) {
-    let firstNotRepeatedCharacter = "";
     for (let i = 0; i < phrase.length; i++) {
         let char = phrase[i];
         if (phrase.indexOf(char) === phrase.lastIndexOf(char)) {
-            firstNotRepeatedCharacter = char;
-            return firstNotRepeatedCharacter;
+            return char;
         }
     }
-    return firstNotRepeatedCharacter;
+    return "";
 }
