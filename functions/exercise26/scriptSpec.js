fd96775522d9395f1e84ecc3ad73d68b51ba1298
Agnieszka Kurek <agniechakurek@gmail.com>
2017-09-06 14:10:36 +0200

    Remove unnecessary spaces and files

--- functions/exercise26/scriptSpec.js
+++ functions/exercise26/scriptSpec.js
@@ -3,7 +3,7 @@
 
 describe(" Find longest substring in a given a string without repeating characters", (() => {
     
-         it("If the string consists of one word 'elephant', then the longest substring without repeating characters is 'lphant' ", (() => {
+         it("If the string consists of one word 'elephant', then the longest substring without repeating characters is 'lphant'", (() => {
              let substringWithoutRepeatingCharacters = getLongestSubstringWithoutRepeatingCharacters("elephant");
              expect(substringWithoutRepeatingCharacters).toEqual("lphant");
          }));
