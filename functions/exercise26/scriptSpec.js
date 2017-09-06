ddeb9f5dfdd064dba5467d169d3ecff3ba191c09
AgnieszkaKurek <agniechakurek@gmail.com>
2017-09-06 18:08:11 +0200

    Feature84 (#93)
    * add new  exercise  folder
    
    * add unit test
    
    * improvement unit test
    
    * changes example
    
    * Remove unnecessary spaces and files
    
    * correcting the test and adding a new example
    
    * rename  and remove empty lines
    
    * adding missing letters

---
+++ functions/exercise26/scriptSpec.js
@@ -0,0 +1,15 @@
+/// <reference path="../../typings/globals/jasmine/index.d.ts" />
+/*globals getLongestSubstringsWithoutRepeatingCharacters*/
+
+describe(" Find longest substring in a given a string without repeating characters", (() => {
+
+    it("If the string consists of one word 'elephant', then the longest substring without repeating characters is 'lephant'", (() => {
+        let substringsWithoutRepeatingCharacters = getLongestSubstringsWithoutRepeatingCharacters("elephant");
+        expect(substringsWithoutRepeatingCharacters).toEqual(["lephant"]);
+    }));
+
+    it("If the string consists of one word 'abba', then the longest substrings without repeating characters are 'ab' 'ba'", (() => {
+        let substringsWithoutRepeatingCharacters = getLongestSubstringsWithoutRepeatingCharacters("abba");
+        expect(substringsWithoutRepeatingCharacters).toEqual(["ab", "ba"]);
+    }));
+}));
