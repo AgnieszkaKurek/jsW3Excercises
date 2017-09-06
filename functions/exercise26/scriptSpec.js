040e4c5b6402c779bda0ed88a171e1f6235e1664
Agnieszka Kurek <agniechakurek@gmail.com>
2017-09-06 15:49:15 +0200

    correcting the test and adding a new example

--- functions/exercise26/scriptSpec.js
+++ functions/exercise26/scriptSpec.js
@@ -3,9 +3,15 @@
 
 describe(" Find longest substring in a given a string without repeating characters", (() => {
     
-         it("If the string consists of one word 'elephant', then the longest substring without repeating characters is 'lphant'", (() => {
+         it("If the string consists of one word 'elephant', then the longest substring without repeating characters is 'lephant'", (() => {
              let substringWithoutRepeatingCharacters = getLongestSubstringWithoutRepeatingCharacters("elephant");
-             expect(substringWithoutRepeatingCharacters).toEqual("lphant");
+             expect(substringWithoutRepeatingCharacters).toEqual(["lephant"]);
          }));
+         
+         it("If the string consists of one word 'abba', then the longest substrings without repeating characters are 'ab' 'ba'", (() => {
+            let substringWithoutRepeatingCharacters = getLongestSubstringWithoutRepeatingCharacters("abba");
+            expect(substringWithoutRepeatingCharacters).toEqual(["ab","ba"]);
+        }));
+    
      
      }));
