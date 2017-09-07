adeff5121b40b9874b6b23ae9780b2cef78dc014
Agnieszka Kurek <agniechakurek@gmail.com>
2017-09-07 13:54:19 +0200

    remove unnecessary example

--- functions/exercise26/scriptSpec.js
+++ functions/exercise26/scriptSpec.js
@@ -14,12 +14,8 @@ describe(" Find longest substring in a given a string without repeating characte
     }));
     it("If the string consists of lowercase and uppercase'aA', then the two  longest substrings without repeating characters are 'a' 'A'", (() => {
         let substringsWithoutRepeatingCharacters = getLongestSubstringsWithoutRepeatingCharacters("aA");
-        expect(substringsWithoutRepeatingCharacters).toEqual(["a","A"]);
+        expect(substringsWithoutRepeatingCharacters).toEqual(["a", "A"]);
     }));
-    
-    it("If string is empty (does not contain any characters) then this is an error", () => {
-        getLongestSubstringsWithoutRepeatingCharacters("").toThrow(new Error("String can not be empty - must contain characters"));
-    });
 
     it("If string is empty (does not contain any characters) then the longest substring without repeating characters is ''", (() => {
         let substringsWithoutRepeatingCharacters = getLongestSubstringsWithoutRepeatingCharacters("");
