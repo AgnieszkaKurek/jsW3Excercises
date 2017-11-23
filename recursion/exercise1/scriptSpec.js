0785e11823f2e9b6c32e575d8548ebf78f43ac75
AgnieszkaKurek <agniechakurek@gmail.com>
2017-11-23 15:39:35 +0100

    rename function and expected value

--- recursion/exercise1/scriptSpec.js
+++ recursion/exercise1/scriptSpec.js
@@ -1,13 +1,13 @@
 /// <reference path="../../typings/globals/jasmine/index.d.ts" />
-/*globals calculateFactorialOfNumber*/
+/*globals factorialOfNumber*/
 
 describe("Calculate the factorial of a number", (() => {
     it("Factorial of 2 is 2", (() => {
-        let factorialOfNumber = calculateFactorialOfNumber(2);
-        expect(factorialOfNumber).toEqual(2);
+        let factorial = factorialOfNumber(2);
+        expect(factorial).toEqual(2);
     }));
     it("Factorial of 5 is 120", (() => {
-        let factorialOfNumber = calculateFactorialOfNumber(5);
-        expect(factorialOfNumber).toEqual(120);
+        let factorial = factorialOfNumber(5);
+        expect(factorial).toEqual(120);
     }));
 }));
