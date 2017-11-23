dfe1cc764d6dd5921f226dd7a58756ca84f21ceb
AgnieszkaKurek <agniechakurek@gmail.com>
2017-11-23 13:37:48 +0100

    correction in the unit test

--- functions/exercise29/scriptSpec.js
+++ functions/exercise29/scriptSpec.js
@@ -3,7 +3,7 @@
 
 describe("Get the function name", (() => {
     it("Function name which get the function name it getTheFunctionName", (() => {
-        let functionName = getTheFunctionName("getTheFunctionName");
-        expect(functionName).toEqual("getTheFunctionName");
+        let functionName = getTheFunctionName(function test(){});
+        expect(functionName).toEqual("test");
     }));
 }));
