a04549bb70a7cc32fe35ea2c54f6abbe5c0efef4
AgnieszkaKurek <agniechakurek@gmail.com>
2017-11-23 11:49:00 +0100

    implementacia function

---
+++ functions/exercise27/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+function getLongestPalindromeInPhrase(phrase) {
+    let words = splitIntoWords(phrase);
+    let palindromes = getAllPalindromes(words);
+    let longestPalindromes = getLongestPalindromes(palindromes);
+    return longestPalindromes;
+}
+
+function splitIntoWords(phrase) {
+    return phrase.split(/[ ,]+/);
+}
+
+function getAllPalindromes(words) {
+    return words.filter(word => checkIsPhraseConsistOfLetterOnly(word) && isPalindrome(word));
+}
+
+function checkIsPhraseConsistOfLetterOnly(phrase) {
+    return /^[a-zA-Z]+$/g.test(phrase);
+}
+
+function isPalindrome(word) {
+    return word.toLowerCase() === word.toLowerCase().split("").reverse().join("");
+}
+
+function getLongestPalindromes(palindromes) {
+    let longestPalindrome = 0;
+    let longestPalindromes = [];
+    for (let palindrome of palindromes) {
+        if (palindrome.length > longestPalindrome) {
+            longestPalindrome = palindrome.length;
+            longestPalindromes = [palindrome];
+        } else if (palindrome.length === longestPalindrome) {
+            longestPalindromes.push(palindrome);
+        }
+    }
+    return longestPalindromes;
+}
