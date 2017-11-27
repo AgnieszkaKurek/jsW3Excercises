2f378dcefaa50a9fff0053c80a020968d720e5dc
AgnieszkaKurek <agniechakurek@gmail.com>
2017-11-27 16:23:22 +0100

    getTheIntegersInRangeExcluding

--- recursion/exercise2/script.js
+++ recursion/exercise2/script.js
@@ -1,16 +1,19 @@
 "use strict";
 
 function findGreatestCommonDivisor(firstNumber, secondNumber) {
-    if (!Number.isInteger(firstNumber) || firstNumber < 1) {
-        throw new Error("Number must be positive integers");
-    }
-
+    validateIfNumberIsPositiveInteger(firstNumber);
+    validateIfNumberIsPositiveInteger(secondNumber);
     while (secondNumber != 0) {
         if (firstNumber > secondNumber)
             firstNumber = firstNumber - secondNumber;
         else
             secondNumber = secondNumber - firstNumber;
     }
-
     return firstNumber;
+}
+
+function validateIfNumberIsPositiveInteger(number) {
+    if (!Number.isInteger(number) || number < 1) {
+        throw new Error("Both numbers must be positive integers");
+    }
 }
--- recursion/exercise3/scriptSpec.js
+++ recursion/exercise3/scriptSpec.js
@@ -1,33 +1,38 @@
 /// <reference path="../../typings/globals/jasmine/index.d.ts" />
-/*globals getTheIntegersInRange*/  
+/*globals getTheIntegersInRangeExcluding*/  
 
-describe("Get the integers in range two numbers", (() => {
-    it("You can not get a result from a range of numbers 3 and 1", (() => {
-        expect(() => getTheIntegersInRange(3, 1)).toThrow(new Error("The first number must be less than or equal to the second"));
+describe("Get the integers in range between two numbers excluding. ", (() => {
+    it("The first number must be less than or equal to the second", (() => {
+        expect(() => getTheIntegersInRangeExcluding(3, 1)).toThrow(new Error("The first number must be less than or equal to the second"));
     }));
 
-    it("It is not possible find  common divisor of 1.2 and 3.5", (() => {
-        expect(() => getTheIntegersInRange(1.2, 3.5)).toThrow(new Error("Number of range must be integers"));
+    it("Number of range must be integers", (() => {
+        expect(() => getTheIntegersInRangeExcluding(1.2, 3.5)).toThrow(new Error("Number of range must be integers"));
     }));
 
-    it("Integers in range for -5 and -2 are -4, -3", (() => {
-        let integersInRange = getTheIntegersInRange(-5, -2);
+    it("If borders are negative, then range is also negative", (() => {
+        let integersInRange = getTheIntegersInRangeExcluding(-5, -2);
         expect(integersInRange).toEqual([-4, -3]);
     }));
 
-    it("Integers in range for -4 and 2 are -3, -2, -1, 0, 1", (() => {
-        let integersInRange = getTheIntegersInRange(-4, 1);
-        expect(integersInRange).toEqual([-3, -2, -1, 0, 1]);
+    it("If first integer is negative and second is positive, then we get range that might have both positive and negative integers", (() => {
+        let integersInRange = getTheIntegersInRangeExcluding(-4, 1);
+        expect(integersInRange).toEqual([-3, -2, -1, 0]);
     }));
 
-    it("Integers in range for 1 and 3 is 2", (() => {
-        let greatestCommonDivisor = getTheIntegersInRange(1, 3);
-        expect(greatestCommonDivisor).toEqual([2]);
+    it("If the difference between borders is 2, then we have only 1 value in a range", (() => {
+        let integersInRange = getTheIntegersInRangeExcluding(1, 3);
+        expect(integersInRange).toEqual([2]);
     }));
 
-    it("Between 3 and 4 is't integer", (() => {
-        let greatestCommonDivisor = getTheIntegersInRange(3, 4);
-        expect(greatestCommonDivisor).toEqual([]);
+    it("If the difference between borders is 1, then we have no values in a range", (() => {
+        let integersInRange = getTheIntegersInRangeExcluding(3, 4);
+        expect(integersInRange).toEqual([]);
+    }));
+
+    it("If borders are the same, then we have no values in a range", (() => {
+        let integersInRange = getTheIntegersInRangeExcluding(3, 3);
+        expect(integersInRange).toEqual([]);
     }));
 
 }));
