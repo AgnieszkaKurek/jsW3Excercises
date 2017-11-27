139269f84025bf2e765e08e03aa4e5bc4871fa55
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2017-11-27 20:47:14 +0100

    Remove console.log that pollutes tests reports

--- basic/exercise7/script.js
+++ basic/exercise7/script.js
@@ -8,6 +8,3 @@ function findAllYearStartingFromSunday(startYear, endYear) {
     }
     return years;
 }
-
-var years = findAllYearStartingFromSunday(2014, 2050);
-console.log(`The first day of ${years} is Sunday`); 
