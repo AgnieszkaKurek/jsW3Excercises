3bd076f50ece6ed0e4e0b837e34bc365875d122d
AgnieszkaKurek <agniechakurek@gmail.com>
2017-11-27 09:45:38 +0100

    implementation function findGreatestCommonDivisor

--- recursion/exercise2/script.js
+++ recursion/exercise2/script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+function findGreatestCommonDivisor(firstNumber, secondNumber) {
+    if (!Number.isInteger(firstNumber) || firstNumber < 1) {
+        throw new Error("Number must be positive integers");
+    }
+
+    while (secondNumber != 0) {
+        if (firstNumber > secondNumber)
+            firstNumber = firstNumber - secondNumber;
+        else
+            secondNumber = secondNumber - firstNumber;
+    }
+
+    return firstNumber;
+}
