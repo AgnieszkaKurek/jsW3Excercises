6255efe3f0429b020206f13f95e9215270a69a96
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2017-11-27 20:56:01 +0100

    Add html reporter

--- karma.conf.js
+++ karma.conf.js
@@ -14,8 +14,11 @@ module.exports = function (config) {
         "plugins": ["transform-es2015-modules-umd"]
       }
     },
+    client:{
+      clearContext: false
+    },
     exclude: [],
-    reporters: ['progress'],
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
