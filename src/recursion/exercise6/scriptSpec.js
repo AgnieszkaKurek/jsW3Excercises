14856bbf9ff15edc1b94d327de9efea29bf48020
AgnieszkaKurek <agniechakurek@gmail.com>
2017-11-30 09:21:13 +0100

    add implementation funtion and changes in unit test

--- src/recursion/exercise6/scriptSpec.js
+++ src/recursion/exercise6/scriptSpec.js
@@ -1,20 +1,46 @@
 import {
-    getFirstFibonacciNumbers
+    getFirstFibonacciNumbersWithWhile,
+    getFirstFibonacciNumbersWithSelfCall
 } from './script';
 
-describe("Get the first Fibonacci numbers.", (() => {
+describe("Get the first Fibonacci numbers using function with while", (() => {
+    it("Number must be greater than 1", (() => {
+        expect(() => getFirstFibonacciNumbersWithWhile(1)).toThrow(new Error("Number must be greater than 1"));
+    }));
+
     it("If Fibonacci numbers contain 2 elements first n are 0, 1", (() => {
-        let firstFibonacciNumbers = getFirstFibonacciNumbers(2);
+        let firstFibonacciNumbers = getFirstFibonacciNumbersWithWhile(2);
         expect(firstFibonacciNumbers).toEqual([0, 1]);
     }));
 
     it("If Fibonacci numbers contain 5 elements first n are 0, 1, 1, 2, 3", (() => {
-        let firstFibonacciNumbers = getFirstFibonacciNumbers(5);
+        let firstFibonacciNumbers = getFirstFibonacciNumbersWithWhile(5);
         expect(firstFibonacciNumbers).toEqual([0, 1, 1, 2, 3]);
     }));
 
     it("If Fibonacci numbers contain 7 elements first n are 0, 1, 1, 2, 3, 5, 8", (() => {
-        let firstFibonacciNumbers = getFirstFibonacciNumbers(7);
+        let firstFibonacciNumbers = getFirstFibonacciNumbersWithWhile(7);
+        expect(firstFibonacciNumbers).toEqual([0, 1, 1, 2, 3, 5, 8]);
+    }));
+}));
+
+describe("Get the first Fibonacci numbers using function with self call", (() => {
+    it("Number must be greater than 1", (() => {
+        expect(() => getFirstFibonacciNumbersWithSelfCall(1)).toThrow(new Error("Number must be greater than 1"));
+    }));
+
+    it("If Fibonacci numbers contain 2 elements first n are 0, 1", (() => {
+        let firstFibonacciNumbers = getFirstFibonacciNumbersWithSelfCall(2);
+        expect(firstFibonacciNumbers).toEqual([0, 1]);
+    }));
+
+    it("If Fibonacci numbers contain 5 elements first n are 0, 1, 1, 2, 3", (() => {
+        let firstFibonacciNumbers = getFirstFibonacciNumbersWithSelfCall(5);
+        expect(firstFibonacciNumbers).toEqual([0, 1, 1, 2, 3]);
+    }));
+
+    it("If Fibonacci numbers contain 7 elements first n are 0, 1, 1, 2, 3, 5, 8", (() => {
+        let firstFibonacciNumbers = getFirstFibonacciNumbersWithSelfCall(7);
         expect(firstFibonacciNumbers).toEqual([0, 1, 1, 2, 3, 5, 8]);
     }));
 }));
