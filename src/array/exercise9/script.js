d76fef3e121eee02ba8b714e4f53f51565e9d475
AgnieszkaKurek <agniechakurek@gmail.com>
2017-12-13 16:12:01 +0100

    Minor improvements

--- src/array/exercise9/script.js
+++ src/array/exercise9/script.js
@@ -1,3 +1,5 @@
 export function changeCaseSize(text) {
-    return text.split("").map(item => item.toUpperCase() === item ? item.toLowerCase() : item.toUpperCase()).join("");
+    return text.split("").map(item =>
+        item.toUpperCase() === item ? item.toLowerCase() : item.toUpperCase()
+    ).join("");
 }
--- src/array/exercise9/scriptSpec.js
+++ src/array/exercise9/scriptSpec.js
@@ -5,19 +5,19 @@ import {
 describe("Change the case of each character in the phrase: ", (() => {
     it("if in the phrase all characters are lowercased, in the formatted phrase all characters are uppercased", () => {
         let formattedPhrase = changeCaseSize("the little pink piggy");
-        expect(formattedPhrase).toEqual("THE LITTLE PINK PIGGY"); 
+        expect(formattedPhrase).toEqual("THE LITTLE PINK PIGGY");
     });
     it("if in the phrase  all characters are uppercased, in the formatted phrase all characters are lowercased", () => {
-        let formattedPhrase =  changeCaseSize("THE BIG BLACK BOAR");
+        let formattedPhrase = changeCaseSize("THE BIG BLACK BOAR");
         expect(formattedPhrase).toEqual("the big black boar");
     });
     it("if in the phrase character are upper and lowercased, uppercase characters change to lowercase and lowercase characters change to uppercase", () => {
-        let formattedPhrase =  changeCaseSize("The Quick Brown Fox");
+        let formattedPhrase = changeCaseSize("The Quick Brown Fox");
         expect(formattedPhrase).toEqual("tHE qUICK bROWN fOX");
     });
     it("white spaces and extra characters are ignored", () => {
-        let formattedPhrase =  changeCaseSize(" aBBa!");
+        let formattedPhrase = changeCaseSize(" aBBa!");
         expect(formattedPhrase).toEqual(" AbbA!");
     });
-    
+
 }));
