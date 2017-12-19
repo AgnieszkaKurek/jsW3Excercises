44db2dd8963764374541eac34c6bf62242a539e4
AgnieszkaKurek <agniechakurek@gmail.com>
2017-12-19 13:14:19 +0100

    Change formatting and replace let with consts

--- src/array/exercise19/script.js
+++ src/array/exercise19/script.js
@@ -1,16 +1,16 @@
-export function  computSumOfEachIndividualElemValue(arrayA, arrayB){
-    let sumOfEachElemValue = [];
-    let arraysLength = Math.max(arrayA.length, arrayB.length);
+export function computSumOfEachIndividualElemValue(arrayA, arrayB) {
+    let sumOfEachElemValues = [];
+    const arraysLength = Math.max(arrayA.length, arrayB.length);
     for (let i = 0; arraysLength > i; i++) {
-        let itemA = arrayA[i];
-        let itemB = arrayB[i];
-       if(itemA && itemB){
-       sumOfEachElemValue.push(itemA + itemB);
-    } else if (itemA && !itemB){
-        sumOfEachElemValue.push(itemA);
-    } else if (itemB && !itemA){
-        sumOfEachElemValue.push(itemB);
+        const itemA = arrayA[i];
+        const itemB = arrayB[i];
+        if (itemA && itemB) {
+            sumOfEachElemValues.push(itemA + itemB);
+        } else if (itemA && !itemB) {
+            sumOfEachElemValues.push(itemA);
+        } else if (itemB && !itemA) {
+            sumOfEachElemValues.push(itemB);
+        }
     }
-    }
-    return sumOfEachElemValue;
+    return sumOfEachElemValues;
 }
--- src/array/exercise19/scriptSpec.js
+++ src/array/exercise19/scriptSpec.js
@@ -2,28 +2,25 @@ import {
     computSumOfEachIndividualElemValue
 } from './script';
 
-
 describe("Compute the sum of each individual index value from the given arrays: ", (() => {
-    
     it("if we add two empty arrays, the result will be an empty array", () => {
-        let sumOfEachElemValue  =  computSumOfEachIndividualElemValue([], []);
+        let sumOfEachElemValue = computSumOfEachIndividualElemValue([], []);
         expect(sumOfEachElemValue).toEqual([]);
     });
     it("if we add empty array and one element array, the result will be one element array", () => {
-        let sumOfEachElemValue  =  computSumOfEachIndividualElemValue([], [3]);
+        let sumOfEachElemValue = computSumOfEachIndividualElemValue([], [3]);
         expect(sumOfEachElemValue).toEqual([3]);
     });
-    it("if we add two one element arrays, the result will one element array", () => {
-        let sumOfEachElemValue  =  computSumOfEachIndividualElemValue([2], [2]);
+    it("if we add two one element arrays, the result will be one element array", () => {
+        let sumOfEachElemValue = computSumOfEachIndividualElemValue([2], [2]);
         expect(sumOfEachElemValue).toEqual([4]);
     });
-    it("if we add two elements array and five elements array, the result will be an five elements array", () => {
-        let sumOfEachElemValue  =  computSumOfEachIndividualElemValue([1, 14], [1, 8, 9, 12, 15]);
+    it("if we add two elements array and five elements array, the result will be a five elements array", () => {
+        let sumOfEachElemValue = computSumOfEachIndividualElemValue([1, 14], [1, 8, 9, 12, 15]);
         expect(sumOfEachElemValue).toEqual([2, 22, 9, 12, 15]);
     });
-    it("if we add five elements array and six elements array, the result will be an six elements array", () => {
-        let sumOfEachElemValue  =  computSumOfEachIndividualElemValue([1, 0, 2, 3, 4], [3, 5, 6, 7, 8, 13]);
+    it("if we add five elements array and six elements array, the result will be a six elements array", () => {
+        let sumOfEachElemValue = computSumOfEachIndividualElemValue([1, 0, 2, 3, 4], [3, 5, 6, 7, 8, 13]);
         expect(sumOfEachElemValue).toEqual([4, 5, 8, 10, 12, 13]);
     });
-   
 }));
