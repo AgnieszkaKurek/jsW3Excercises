284a666c4bf0aacb320183f22e2ae9d1a7c0970d
AgnieszkaKurek <agniechakurek@gmail.com>
2017-12-22 17:57:01 +0100

    add  changes

--- src/array/exercise22/scriptSpec.js
+++ src/array/exercise22/scriptSpec.js
@@ -1,27 +1,27 @@
 
 import {
-    computeTheUnionOfTwoArrays
+    mergeAndSort
 } from './script';
 
 describe("Compute the union of two arrays: ", (() => {
-    it("result union of two empty arrays, is empty array", () => {
-        let unionArrays = computeTheUnionOfTwoArrays([], []);
+    it("two empty arrays compute to an empty array", () => {
+        let unionArrays = mergeAndSort([], []);
         expect(unionArrays).toEqual([]);
     });
-    it("result union of empty array and three-elements array, is three-elements array", () => {
-        let unionArrays = computeTheUnionOfTwoArrays([], [100, 2, 1]);
+    it("empty array and non empty array compute to sorted array of items from second array", () => {
+        let unionArrays = mergeAndSort([], [100, 2, 1]);
         expect(unionArrays).toEqual([1, 2, 100]);
     });
-    it("result union of two arrays with the same length and and the same elements, is the same array", () => {
-        let unionArrays = computeTheUnionOfTwoArrays([1], [1]);
+    it("cumputation of two the same arrays is the same array", () => {
+        let unionArrays = mergeAndSort([1], [1]);
         expect(unionArrays).toEqual([1]);
     });
-    it("result union of two array with the same length but different elements, is two-elements array", () => {
-        let unionArrays = computeTheUnionOfTwoArrays([10], [100]);
+    it("result of the union of two arrays with the same length but different elements, is two-elements array", () => {
+        let unionArrays = mergeAndSort([10], [100]);
         expect(unionArrays).toEqual([10, 100]);
     });
-    it("result union of two array whit different lenght and the same and different array , is array in which there are elements from two array, but the same elements occur only once", () => {
-        let unionArrays = computeTheUnionOfTwoArrays([1, 2, 3], [100, 2, 1, 10]);
+    it("result array should have unique sorted items", () => {
+        let unionArrays = mergeAndSort([1, 2, 3], [100, 2, 1, 10]);
         expect(unionArrays).toEqual([1, 2, 3, 10, 100]);
     });
 }));
