db0750df6cc2fb2c7cc7403fbc78dd9b7b0ecffe
AgnieszkaKurek <agniechakurek@gmail.com>
2017-12-29 11:17:19 +0100

    use class in function  sortbyTitle

--- src/array/exercise25/script.js
+++ src/array/exercise25/script.js
@@ -1,12 +1,14 @@
-export class createBook {
-    constructor(author, title, ibraryID) {
+export class Book {
+    constructor(author, title, libraryId) {
         this.author = author;
         this.title = title;
-        this.ibraryID = ibraryID;
+        this.libraryId = libraryId;
+    }
+    compareByTitle(another) {
+        return this.title > another.title;
     }
 }
 
 export function sortByTitle(books) {
-    return books.sort((a, b) => a.title > b.title);
+    return books.sort((a, b) => a.compareByTitle(b));
 }
-
--- src/array/exercise25/scriptSpec.js
+++ src/array/exercise25/scriptSpec.js
@@ -1,16 +1,12 @@
 import {
-    sortByTitle, createBook
+    sortByTitle, Book
 } from './script';
 
 describe('Sort the following array of objects by title value', () => {
     it('Should sort book by title value', () => {
-        let books = [];
-        books.push(new createBook('Bill Gates', 'The Road Ahead', 1254), new createBook('Steve Jobs', 'Walter Isaacson', 4264), new createBook('Suzanne Collins', 'Mockingjay: The Final Book of The Hunger Games', 3245));
-
-        let orderedBooks = sortByTitle(books);
-        expect(orderedBooks).toEqual([new createBook('Suzanne Collins', 'Mockingjay: The Final Book of The Hunger Games', 3245), new createBook('Bill Gates', 'The Road Ahead', 1254), new createBook('Steve Jobs', 'Walter Isaacson', 4264)]
+        let book = [new Book('Bill Gates', 'The Road Ahead', 1254), new Book('Steve Jobs', 'Walter Isaacson', 4264), new Book('Suzanne Collins', 'Mockingjay: The Final Book of The Hunger Games', 3245)];
+        let orderedBook = sortByTitle(book);
+        expect(orderedBook).toEqual([new Book('Suzanne Collins', 'Mockingjay: The Final Book of The Hunger Games', 3245), new Book('Bill Gates', 'The Road Ahead', 1254), new Book('Steve Jobs', 'Walter Isaacson', 4264)]
         );
     });
 });
-
-
