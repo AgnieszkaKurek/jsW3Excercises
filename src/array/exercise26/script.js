c10e3cc95f1c5e079b0d94c8bfe3aa59c9c98655
AgnieszkaKurek <agniechakurek@gmail.com>
2017-12-29 07:21:09 +0100

    getPositionOfPairsWithSpecifiedSum

--- src/array/exercise26/script.js
+++ src/array/exercise26/script.js
@@ -1,9 +1,33 @@
+export class PositionPair {
+    constructor(formerPosition, latterPosition) {
+        this.formerPosition = formerPosition;
+        this.latterPosition = latterPosition;
+    }
+
+    compare(another) {
+        if (this.formerPosition > another.formerPosition) {
+            return 1;
+        } else if (this.formerPosition === another.formerPosition) {
+            return this.latterPosition - another.latterPosition;
+        } else {
+            return -1;
+        }
+    }
+
+    equals(another) {
+        return this.compare(another) === 0;
+    }
+}
+
 export function getPositionOfPairsWithSpecifiedSum(array, targetNumber) {
-    const result = [];
+    if (array.length < 2) {
+        throw new Error("Array should have at least 2 items");
+    }
+    let result = new Set();
     for (let i = 0; i < array.length; i++) {
         for (let j = i + 1; j < array.length; j++) {
             if (array[i] + array[j] === targetNumber) {
-                result.push(i + 1, j + 1);
+                result.add(new PositionPair(i + 1, j + 1));
             }
         }
     }
--- src/array/exercise26/scriptSpec.js
+++ src/array/exercise26/scriptSpec.js
@@ -1,11 +1,15 @@
 import {
-    getPositionOfPairsWithSpecifiedSum
+    getPositionOfPairsWithSpecifiedSum, PositionPair
 } from './script';
+
+function convertToSortedArray(setOfComparableItems){
+    return Array.from(setOfComparableItems).sort((a, b) => a.compare(b));
+}
 
 describe("Find pairs of items from a given array whose sum equals a specific target number: ", (() => {
     it("positions are counted from 1", () => {
         let result = getPositionOfPairsWithSpecifiedSum([10, 20], 30);
-        expect(result).toEqual(new Set([{ formerPosition: 1, latterPosition: 2 }]));
+        expect(result).toEqual(new Set([new PositionPair(1, 2)]));
     });
     it("array should have at least 2 items", () => {
         expect(() => getPositionOfPairsWithSpecifiedSum([20], 20)).toThrow(new Error("Array should have at least 2 items"));
@@ -16,11 +20,18 @@ describe("Find pairs of items from a given array whose sum equals a specific tar
     });
     it("if there are two matching pairs, we should return both of them", () => {
         let result = getPositionOfPairsWithSpecifiedSum([50, 10, 20, 60, 30, 40], 50);
-        expect(result).toEqual(new Set(
-            [
-                { formerPosition: 2, latterPosition: 6 },
-                { formerPosition: 3, latterPosition: 5 }
-            ]
-        ));
+        expect(result instanceof Set).toBe(true);
+        expect(result.size).toBe(2);
+        let sortedResults = convertToSortedArray(result);
+        expect(sortedResults[0].equals(new PositionPair(2, 6))).toBe(true);
+        expect(sortedResults[1].equals(new PositionPair(3, 5))).toBe(true);
+    });
+    it("one item might use the same position twice", () => {
+        let result = getPositionOfPairsWithSpecifiedSum([10, 10, 40], 50);
+        expect(result instanceof Set).toBe(true);
+        expect(result.size).toBe(2);
+        let sortedResults = convertToSortedArray(result);
+        expect(sortedResults[0].equals(new PositionPair(1, 3))).toBe(true);
+        expect(sortedResults[1].equals(new PositionPair(2, 3))).toBe(true);
     });
 }));
