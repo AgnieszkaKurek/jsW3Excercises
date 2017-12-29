36f1dd57d5ebb00c97ea587e7efa266a37e74af4
AgnieszkaKurek <agniechakurek@gmail.com>
2017-12-29 11:20:34 +0100

    exercise25-use class (#123)
    * changes in test and implementation - use class creat Book
    
    * use class in function  sortbyTitle

--- src/array/exercise25/script.js
+++ src/array/exercise25/script.js
@@ -1,4 +1,14 @@
-export function sortByTitle(books) {
-    return books.sort((a, b) => a.title > b.title);
+export class Book {
+    constructor(author, title, libraryId) {
+        this.author = author;
+        this.title = title;
+        this.libraryId = libraryId;
+    }
+    compareByTitle(another) {
+        return this.title > another.title;
+    }
 }
 
+export function sortByTitle(books) {
+    return books.sort((a, b) => a.compareByTitle(b));
+}
--- src/array/exercise25/scriptSpec.js
+++ src/array/exercise25/scriptSpec.js
@@ -1,45 +1,12 @@
 import {
-    sortByTitle
+    sortByTitle, Book
 } from './script';
 
 describe('Sort the following array of objects by title value', () => {
     it('Should sort book by title value', () => {
-        let books = [{
-            author: 'Bill Gates',
-            title: 'The Road Ahead',
-            libraryID: 1254
-        },
-        {
-            author: 'Steve Jobs',
-            title: 'Walter Isaacson',
-            libraryID: 4264
-        },
-        {
-            author: 'Suzanne Collins',
-            title: 'Mockingjay: The Final Book of The Hunger Games',
-            libraryID: 3245
-        },
-        ];
-        let orderedBooks = sortByTitle(books);
-        expect(orderedBooks).toEqual(
-            [{
-                author: 'Suzanne Collins',
-                title: 'Mockingjay: The Final Book of The Hunger Games',
-                libraryID: 3245
-            },
-            {
-                author: 'Bill Gates',
-                title: 'The Road Ahead',
-                libraryID: 1254
-            },
-            {
-                author: 'Steve Jobs',
-                title: 'Walter Isaacson',
-                libraryID: 4264
-            },
-            ]
+        let book = [new Book('Bill Gates', 'The Road Ahead', 1254), new Book('Steve Jobs', 'Walter Isaacson', 4264), new Book('Suzanne Collins', 'Mockingjay: The Final Book of The Hunger Games', 3245)];
+        let orderedBook = sortByTitle(book);
+        expect(orderedBook).toEqual([new Book('Suzanne Collins', 'Mockingjay: The Final Book of The Hunger Games', 3245), new Book('Bill Gates', 'The Road Ahead', 1254), new Book('Steve Jobs', 'Walter Isaacson', 4264)]
         );
     });
 });
-
-
