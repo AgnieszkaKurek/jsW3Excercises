fd3785c1a21e1d8f41d618fe1278108bfd141621
AgnieszkaKurek <agniechakurek@gmail.com>
2018-01-02 17:28:59 +0100

    Improve algorithm

--- src/array/exercise28/script.js
+++ src/array/exercise28/script.js
@@ -1,15 +1,21 @@
-export function longestCommonStartingSubstring(stringArray) {
-    if (stringArray.length === 0) {
-        return "";
+export function longestCommonStartingSubstring(arr) {
+    if (arr.length === 0) return "";
+    let substringCandidate = arr.reduce((a, b) => a.length <= b.length ? a : b);
+    while (substringCandidate) {
+        if (allItemsStartWithTheSameSubstringCandidate(arr, substringCandidate)) {
+            return substringCandidate;
+        } else {
+            substringCandidate = substringCandidate.slice(0, -1);
+        }
     }
-    const sortedStringArray = stringArray.sort();
-    const firstString = sortedStringArray[0];
-    const lastString = sortedStringArray[sortedStringArray.length - 1];
-    const firstStringLenght = firstString.length;
-    let i = 0;
-    while (i < firstStringLenght && firstString.charAt(i) === lastString.charAt(i)) {
-        i++;
-    }
-    return firstString.substring(0, i);
+    return "";
 }
 
+function allItemsStartWithTheSameSubstringCandidate(stringArray, substringCandidate) {
+    for (const item of stringArray) {
+        if (!item.startsWith(substringCandidate)) {
+            return false;
+        }
+    }
+    return true;
+}
