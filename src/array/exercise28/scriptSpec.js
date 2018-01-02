4f8f02cef31df40f0b8e4d817c294d3993c08976
AgnieszkaKurek <agniechakurek@gmail.com>
2018-01-02 08:31:21 +0100

    changing the name of a variable called the same as function

--- src/array/exercise28/scriptSpec.js
+++ src/array/exercise28/scriptSpec.js
@@ -4,22 +4,22 @@ import {
 
 describe("Find the longest common starting substring in a set of strings: ", (() => {
     it("if no strings provided then common substring is empty", () => {
-        let longestCommonStartingSubstring = longestCommonStartingSubstring([]);
-        expect(longestCommonStartingSubstring).toEqual("");
+        let result =  longestCommonStartingSubstring([]);
+        expect(result).toEqual("");
     });
 
     it("if one string provided, it is the longest substring", () => {
-        let longestCommonStartingSubstring = longestCommonStartingSubstring(["pig"]);
-        expect(longestCommonStartingSubstring).toEqual("pig");
+        let result =  longestCommonStartingSubstring(["pig"]);
+        expect(result).toEqual("pig");
     });
 
     it("if many substrings provided, get maximum substring for all of them", () => {
-        let longestCommonStartingSubstring = longestCommonStartingSubstring(["bee", "be", "beer", "bear"]);
-        expect(longestCommonStartingSubstring).toEqual("be");
+        let result =  longestCommonStartingSubstring(["bee", "be", "beer", "bear"]);
+        expect(result).toEqual("be");
     });
     it("if strings do not start with the same, return empty substring", () => {
-        let longestCommonStartingSubstring = longestCommonStartingSubstring(["ab", "cd", "abb"]);
-        expect(longestCommonStartingSubstring).toEqual("");
+        let result =  longestCommonStartingSubstring(["ab", "cd", "abb"]);
+        expect(result).toEqual("");
     });
 
 }));
