265c7fc9d7e3438a733f3fac26aa0f9878447a84
AgnieszkaKurek <agniechakurek@gmail.com>
2018-01-02 16:52:28 +0100

    Initial changes

---
+++ src/array/exercise29/scriptSpec.js
@@ -0,0 +1,15 @@
+import {
+   fillArrayWithValues
+} from './script';
+
+describe("Fill an array with values (numeric, string with one character) on supplied bounds: ", (() => {
+    it("", () => {
+        let newArray = fillArrayWithValues();
+        expect(newArray).toEqual([]);
+    });
+    it("", () => {
+        let newArray  = fillArrayWithValues();
+        expect(newArray).toEqual([]);
+    });
+    
+}));
