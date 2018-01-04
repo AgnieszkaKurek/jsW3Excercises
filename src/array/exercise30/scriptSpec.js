ff5a90451813eb8e8c146c817e482637b6ca8781
AgnieszkaKurek <agniechakurek@gmail.com>
2018-01-04 09:53:54 +0100

    formating file

---
+++ src/array/exercise30/scriptSpec.js
@@ -0,0 +1,22 @@
+import {
+    mergeArraysAndRemovesDuplicates
+} from './script';
+
+describe("Merge two arrays and removes all duplicates elements: ", (() => {
+    it("if two arrays are empty, result is to empty array", () => {
+        const result = mergeArraysAndRemovesDuplicates([], []);
+        expect(result).toEqual([]);
+    });
+    it("if in arrays all elements are duplicates, result is array where all duplicates are removes ", () => {
+        const result = mergeArraysAndRemovesDuplicates([1, 2, 3], [3, 2, 1]);
+        expect(result).toEqual([1, 2, 3]);
+    });
+    it("if in arrays all elements are unique, result merge array haves all elements", () => {
+        const result = mergeArraysAndRemovesDuplicates([1, 2, 3], [4, 5, 6]);
+        expect(result).toEqual([1, 2, 3, 4, 5, 6]);
+    });
+    it("if in arrays all elements are unique, result merge array haves all elements", () => {
+        const result = mergeArraysAndRemovesDuplicates([1, 2, 3], [2, 30, 1]);
+        expect(result).toEqual([1, 2, 3, 30]);
+    });
+}));
