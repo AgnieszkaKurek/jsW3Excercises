401fa370310b64b89928ab36b42a67ffb96c5c3f
AgnieszkaKurek <agniechakurek@gmail.com>
2018-01-05 15:29:01 +0100

    moveElemFromOneIndexToAnother implementation

--- src/array/exercise38/script.js
+++ src/array/exercise38/script.js
@@ -1,3 +1,19 @@
 export function moveElemFromOneIndexToAnother(arr, source, destination) {
-    throw new Error("Not implemented yet");
+    if ((source < 0 && destination >= 0) || (source >= 0 && destination < 0)) throw new Error("if one position is negative, then second also has must be negative");
+    validateIfPositionExistsInArr(arr, source);
+    validateIfPositionExistsInArr(arr, destination);
+    if (source < 0 && destination < 0) {
+        source = arr.length + source;
+        destination = arr.length + destination;
+    }
+    const valueToMove = arr[source];
+    arr.splice(source, 1);
+    arr.splice(destination, 0, valueToMove);
+    return arr;
+}
+
+function validateIfPositionExistsInArr(arr, position) {
+    if ((position < 0 ? Math.abs(position + 1) : position) >= arr.length) {
+        throw new Error("source and destination positons must be present in the array");
+    }
 }
--- src/array/exercise38/scriptSpec.js
+++ src/array/exercise38/scriptSpec.js
@@ -4,10 +4,22 @@ import {
 
 describe("Move an array element from one position to another:", (() => {
     it("source positon must be present in the array", () => {
-        expect(() => moveElemFromOneIndexToAnother([1, 2], 3, 0)).toThrow(new Error("Source positon must be present in the array"));
+        expect(() => moveElemFromOneIndexToAnother([1, 2], 3, 0)).toThrow(new Error("source and destination positons must be present in the array"));
     });
     it("destination positon must be present in the array", () => {
-        expect(() => moveElemFromOneIndexToAnother([1, 2], 0, 3)).toThrow(new Error("Destination positon must be present in the array"));
+        expect(() => moveElemFromOneIndexToAnother([1, 2], 0, 3)).toThrow(new Error("source and destination positons must be present in the array"));
+    });
+    it("negative source positon must be present in the array", () => {
+        expect(() => moveElemFromOneIndexToAnother([1, 2], -3, -1)).toThrow(new Error("source and destination positons must be present in the array"));
+    });
+    it("negative destination positon must be present in the array", () => {
+        expect(() => moveElemFromOneIndexToAnother([1, 2], -1, -3)).toThrow(new Error("source and destination positons must be present in the array"));
+    });
+    it("source and destination must be of the same sign", () => {
+        expect(() => moveElemFromOneIndexToAnother([1, 2], -1, 1)).toThrow(new Error("if one position is negative, then second also has must be negative"));
+    });
+    it("negative position must be used only with another negative position", () => {
+        expect(() => moveElemFromOneIndexToAnother([1, 2], -1, 0)).toThrow(new Error("if one position is negative, then second also has must be negative"));
     });
     it("positions are counted from 0", () => {
         const changedArray = moveElemFromOneIndexToAnother([1, 2], 0, 1);
@@ -28,5 +40,9 @@ describe("Move an array element from one position to another:", (() => {
     it("negative position means that we count index from the rigth side", () => {
         const changedArray = moveElemFromOneIndexToAnother([1, 2, 3, 4], -1, -3);
         expect(changedArray).toEqual([1, 4, 2, 3]);
+    });
+    it("using negative position we may move item to the end", () => {
+        const changedArray = moveElemFromOneIndexToAnother([1, 2, 3, 4], -3, -1);
+        expect(changedArray).toEqual([1, 3, 4, 2]);
     });
 }));
