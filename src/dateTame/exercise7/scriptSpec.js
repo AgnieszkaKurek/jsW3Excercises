d8bc92532b3e5b4855701e77f847642aa0f15ca0
AgnieszkaKurek <agniechakurek@gmail.com>
2018-01-10 13:06:14 +0100

    add unit test to exercise 7 in folderDataTime

---
+++ src/dateTame/exercise7/scriptSpec.js
@@ -0,0 +1,15 @@
+import {
+    testDateIsWeekend
+} from './script';
+
+describe("Test whether a date is a weekend:", (() => {
+    it("if date is weekend, return true ", () => {
+        let result = testDateIsWeekend("Nov 15, 2014");
+        expect(result).toBe(true);
+    });
+    it("if date isn't weekend, return false", () => {
+        let result = testDateIsWeekend("Nov 17, 2014");
+        expect(result).toBe(false);
+    });
+
+}));
