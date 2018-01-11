8315b3a05bba168e062f275ebe0bc47b44e47d9b
AgnieszkaKurek <agniechakurek@gmail.com>
2018-01-11 17:37:52 +0100

    Fix implementation

--- src/dateTime/exercise5/script.js
+++ src/dateTime/exercise5/script.js
@@ -1,5 +1,5 @@
 export function compareDates(dateA, dateB) {
-    if (dateA === dateB) return "dateA = dateB";
-    else if (dateA > dateB) return "dateA > dateB";
-    else return "dateA < dateB";
+    if (dateA.getTime() === dateB.getTime()) return 0;
+    if (dateA > dateB) return 1;
+    return -1;
 } 
--- src/dateTime/exercise5/scriptSpec.js
+++ src/dateTime/exercise5/scriptSpec.js
@@ -2,17 +2,17 @@ import {
     compareDates
 } from './script';
 
-describe("Compare two dates (i.e. greater than, less than or equal to):", (() => {
-    it("if the two dates are the same, they are equal", () => {
-        let result = compareDates("2013/11/14 00:00", "2013/11/14 00:00");
-        expect(result).toEqual("dateA = dateB");
+describe("Compare two dates", (() => {
+    it("if two dates are the same, return 0", () => {
+        let result = compareDates(new Date('11/14/2013 00:00'), new Date('11/14/2013 00:00'));
+        expect(result).toEqual(0);
     });
-    it("if first data is the second after second date, first date is greater than second ", () => {
-        let result = compareDates("2013/11/14 00:01", "2013/11/14 00:00");
-        expect(result).toEqual("dateA > dateB");
+    it("if first data is bigger than second data, return 1", () => {
+        let result = compareDates(new Date('11/14/2013 00:01'), new Date('11/14/2013 00:00'));
+        expect(result).toEqual(1);
     });
-    it("if second data is the second after first date, first date is less than second ", () => {
-        let result = compareDates("2013/11/14 00:00", "2013/11/14 00:01");
-        expect(result).toEqual("dateA < dateB");
+    it("if first data is smaller than second data, return -1", () => {
+        let result = compareDates(new Date('11/14/2013 00:00'), new Date('11/14/2013 00:01'));
+        expect(result).toEqual(-1);
     });
 }));
