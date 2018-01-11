87b1bc73cd4dc6f6e390d54d92729a5910c39ed9
AgnieszkaKurek <agniechakurek@gmail.com>
2018-01-11 10:38:47 +0100

    add unit test for function  getTheMonthNameFromDate

---
+++ src/dateTime/exercise4/scriptSpec.js
@@ -0,0 +1,18 @@
+import {
+    getTheMonthNameFromDate
+} from './script';
+
+describe("Get the month name from a particular date:", (() => {
+    it("eleventh month is October", () => {
+        let monthName = getTheMonthNameFromDate("10/11/2009");
+        expect(monthName).toEqual("October");
+    });
+    it("fourth month is April", () => {
+        let monthName = getTheMonthNameFromDate("20/04/1986");
+        expect(monthName).toEqual("April");
+    });
+    it("first month is January", () => {
+        let monthName = getTheMonthNameFromDate("05/01/2018");
+        expect(monthName).toEqual("January");
+    });
+})); 
