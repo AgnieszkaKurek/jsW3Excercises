7adf2e398424c0868eb85b538844e6e69838ecb1
AgnieszkaKurek <agniechakurek@gmail.com>
2018-01-11 17:22:03 +0100

    Fix implementation

--- src/dateTime/exercise4/script.js
+++ src/dateTime/exercise4/script.js
@@ -1,7 +1,5 @@
 export function getTheMonthNameFromDate(date) {
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    const dateObject = { "production_date": date };
-    const datailedDate = new Date(dateObject.production_date);
-    let month = datailedDate.getMonth();
+    let month = date.getMonth();
     return months[month];
 }
--- src/dateTime/exercise4/scriptSpec.js
+++ src/dateTime/exercise4/scriptSpec.js
@@ -3,16 +3,12 @@ import {
 } from './script';
 
 describe("Get the month name from a particular date:", (() => {
+    it("tenth month is October", () => {
+        let monthName = getTheMonthNameFromDate(new Date("10/11/2009"));
+        expect(monthName).toEqual("October");
+    });
     it("eleventh month is November", () => {
-        let monthName = getTheMonthNameFromDate("2009/11/10");
+        let monthName = getTheMonthNameFromDate(new Date("11/13/2014"));
         expect(monthName).toEqual("November");
-    });
-    it("fourth month is April", () => {
-        let monthName = getTheMonthNameFromDate("1986/04/20");
-        expect(monthName).toEqual("April");
-    });
-    it("first month is January", () => {
-        let monthName = getTheMonthNameFromDate("2018/01/05");
-        expect(monthName).toEqual("January");
     });
 })); 
