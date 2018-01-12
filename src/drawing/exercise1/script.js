6603cb05b051468c6b10f3c9ebe9a49417c27a89
AgnieszkaKurek <agniechakurek@gmail.com>
2018-01-12 15:42:19 +0100

    add color style

--- src/drawing/exercise1/script.js
+++ src/drawing/exercise1/script.js
@@ -1,5 +1,6 @@
 const canvasElem = document.getElementById("canvas");
 const ctx = canvasElem.getContext("2d");
+ctx.fillStyle = "#6DCF00";
 ctx.fillRect(20, 20, 100, 100);
 ctx.clearRect(40, 40, 60, 60);
 ctx.strokeRect(45, 45, 50, 50);
