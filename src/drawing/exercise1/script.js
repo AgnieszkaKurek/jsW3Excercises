9b2a9d7b88c5449e45ad54cb86b78dd44895f188
AgnieszkaKurek <agniechakurek@gmail.com>
2018-01-12 15:05:40 +0100

    add more complicated  shape

--- src/drawing/exercise1/script.js
+++ src/drawing/exercise1/script.js
@@ -1,5 +1,6 @@
-const  canvasElem = document.getElementById("canvas");
+const canvasElem = document.getElementById("canvas");
 const ctx = canvasElem.getContext("2d");
-ctx.rect(100,100,100,100);
-ctx .stroke();
+ctx.fillRect(20, 20, 100, 100);
+ctx.clearRect(40, 40, 60, 60);
+ctx.strokeRect(45, 45, 50, 50);
 
