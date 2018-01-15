557ae44f7771d656758dee1dc7615ace445a8694
AgnieszkaKurek <agniechakurek@gmail.com>
2018-01-15 15:24:45 +0100

    add exercise 5 and index.html and implementation

--- src/drawing/exercise5/script.js
+++ src/drawing/exercise5/script.js
@@ -1,13 +1,10 @@
 const canvasElem = document.getElementById("canvas");
 const ctx = canvasElem.getContext("2d");
 ctx.beginPath();
-   // Outer circle
-    ctx.arc(75,75,50,0,Math.PI*2,true);
-    ctx.moveTo(110,75);
-    // Mouth 
-    ctx.arc(75,75,35,0,Math.PI,false); 
-    // Lefy and Right eye
-    ctx.moveTo(55,65);
-    ctx.arc(60,65,5,0,Math.PI*2,true); 
-    ctx.arc(90,65,5,0,Math.PI*2,true);
-    ctx.stroke();
+ctx.arc(75, 75, 50, 0, Math.PI * 2, true);
+ctx.moveTo(110, 75);
+ctx.arc(75, 75, 35, 0, Math.PI, false);
+ctx.moveTo(55, 65);
+ctx.arc(60, 65, 5, 0, Math.PI * 2, true);
+ctx.arc(90, 65, 5, 0, Math.PI * 2, true);
+ctx.stroke();
