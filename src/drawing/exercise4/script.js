86a1e26537d8febdb07ef2816055add58cc01886
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2018-01-15 17:33:01 +0100

    Fix formatting

--- src/drawing/exercise4/script.js
+++ src/drawing/exercise4/script.js
@@ -1,7 +1,7 @@
 const canvasElem = document.getElementById("canvas");
 const ctx = canvasElem.getContext("2d");
 ctx.beginPath();
-ctx.moveTo(175,175);
+ctx.moveTo(175, 175);
 ctx.lineTo(110, 175);
 ctx.lineTo(110, 125);
 ctx.fill();
