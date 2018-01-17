211e4ca152e76668574eb1a7818a06f630ca6813
AgnieszkaKurek <agniechakurek@gmail.com>
2018-01-17 09:06:58 +0100

    Drawing/3 (#141)
    * add fo;lder exercise3
    
    * add index .html
    
    * add two intersecting rectangles
    
    * Fix formatting

---
+++ src/drawing/exercise3/script.js
@@ -0,0 +1,6 @@
+const canvasElem = document.getElementById("canvas");
+const ctx = canvasElem.getContext("2d");
+ctx.fillStyle = "rgb(256,0,0)";
+ctx.fillRect(50, 50, 100, 100);
+ctx.fillStyle = "rgba(0, 0, 200, 0.6)";
+ctx.fillRect(70, 70, 100, 100); 
